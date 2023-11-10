1a99d4840550ba778ab52c47673e0c10e56ffc90
yonghwankim.dev <dragonbead95@naver.com>
2023-11-10 14:25:06 +0900

    #4 feat: 메소드 로깅 aop 구현

---
+++ src/main/java/codesquad/fineants/spring/aop/SimpleLogAop.java
@@ -0,0 +1,57 @@
+package codesquad.fineants.spring.aop;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Aspect
+@Component
+public class SimpleLogAop {
+
+	@Pointcut("execution(* codesquad.fineants..*.*(..))")
+	private void cut() {
+	}
+
+	@Before("cut()")
+	public void beforeParameterLog(JoinPoint joinPoint) {
+		Method method = getMethod(joinPoint);
+		log.info("======= method name = {} =======", method.getName());
+
+		Object[] args = joinPoint.getArgs();
+		if (args.length <= 0) {
+			log.info("no parameter");
+		}
+		Arrays.stream(args).forEach(arg -> {
+			log.info("parameter type = {}", arg.getClass().getSimpleName());
+			log.info("parameter value = {}", arg);
+		});
+	}
+
+	@AfterReturning(value = "cut()", returning = "returnObj")
+	public void afterReturnLog(JoinPoint joinPoint, Object returnObj) {
+		if (returnObj == null) {
+			return;
+		}
+
+		Method method = getMethod(joinPoint);
+		log.info("======= method name = {} =======", method.getName());
+
+		log.info("return type = {}", returnObj.getClass().getSimpleName());
+		log.info("return value = {}", returnObj);
+	}
+
+	private Method getMethod(JoinPoint joinPoint) {
+		MethodSignature signature = (MethodSignature)joinPoint.getSignature();
+		return signature.getMethod();
+	}
+}
--- src/main/java/codesquad/fineants/spring/api/kis/manager/KisAccessTokenManager.java
+++ src/main/java/codesquad/fineants/spring/api/kis/manager/KisAccessTokenManager.java
@@ -10,7 +10,7 @@ import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
 @Component
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class KisAccessTokenManager {
 	private String accessToken;
