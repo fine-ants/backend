4dcec9eac1d918658cdddc0204c5cd7bc024242f
YongHwan Kim <dragonbead95@naver.com>
2023-11-13 17:41:14 +0900

    [fix] 포트폴리오 수익 히스토리 생성 메소드에 @Transactional 추가 (#12)
    * #4 fix: 인가 코드 요청 URL 경로 변경
    
    - /api/auth/:provider/url -> /api/auth/:provider/authUrl
    
    * #4 feat: 로그 추가
    
    - 네이버 소셜 로그인 버그를 위한 로그 추가
    
    * #4 fix: 포트폴리오 수익 히스토리 생성 메소드에 @Transactional 추가

--- src/main/java/codesquad/fineants/spring/api/portfolio_gain_history/PortfolioGainHistoryService.java
+++ src/main/java/codesquad/fineants/spring/api/portfolio_gain_history/PortfolioGainHistoryService.java
@@ -2,9 +2,7 @@ package codesquad.fineants.spring.api.portfolio_gain_history;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -14,9 +12,6 @@ import codesquad.fineants.domain.portfolio.Portfolio;
 import codesquad.fineants.domain.portfolio.PortfolioRepository;
 import codesquad.fineants.domain.portfolio_gain_history.PortfolioGainHistory;
 import codesquad.fineants.domain.portfolio_gain_history.PortfolioGainHistoryRepository;
-import codesquad.fineants.domain.portfolio_holding.PortfolioHolding;
-import codesquad.fineants.domain.stock.Stock;
-import codesquad.fineants.spring.api.kis.KisService;
 import codesquad.fineants.spring.api.kis.manager.CurrentPriceManager;
 import codesquad.fineants.spring.api.portfolio_gain_history.response.PortfolioGainHistoryCreateResponse;
 import lombok.RequiredArgsConstructor;
@@ -29,9 +24,9 @@ import lombok.extern.slf4j.Slf4j;
 public class PortfolioGainHistoryService {
 	private final PortfolioGainHistoryRepository repository;
 	private final PortfolioRepository portfolioRepository;
-	private final KisService kisService;
 	private final CurrentPriceManager currentPriceManager;
 
+	@Transactional
 	@Scheduled(cron = "0 0 16 * * ?") // 매일 16시에 실행
 	public void scheduledPortfolioGainHistory() {
 		PortfolioGainHistoryCreateResponse response = addPortfolioGainHistory();
@@ -52,15 +47,5 @@ public class PortfolioGainHistoryService {
 		}
 
 		return PortfolioGainHistoryCreateResponse.from(portfolioGainHistories);
-	}
-
-	private static List<String> readTickerSymbols(List<Portfolio> portfolios) {
-		List<String> tickerSymbols = portfolios.stream()
-			.map(Portfolio::getPortfolioHoldings)
-			.flatMap(Collection::stream)
-			.map(PortfolioHolding::getStock)
-			.map(Stock::getTickerSymbol)
-			.collect(Collectors.toList());
-		return tickerSymbols;
 	}
 }
