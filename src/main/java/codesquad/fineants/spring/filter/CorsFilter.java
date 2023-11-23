f27ff9c03bf974d3722e07a4939a1cd283865659
jonghne <jh13012@gmail.com>
2023-11-23 21:20:12 +0900

    #15 fix : merge 중 spring security 라이브러리 누락된 부분 추가

--- src/main/java/codesquad/fineants/spring/filter/CorsFilter.java
+++ src/main/java/codesquad/fineants/spring/filter/CorsFilter.java
@@ -17,8 +17,8 @@ import org.springframework.stereotype.Component;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@Component
-@Order(Ordered.HIGHEST_PRECEDENCE)
+//@Component
+//@Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsFilter implements Filter {
 
 	@Override
