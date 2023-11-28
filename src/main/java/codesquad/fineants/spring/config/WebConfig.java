5a580fcbc645ca846457741ddec68c12584d3112
yonghwankim-dev <dragonbead95@naver.com>
2023-11-28 16:36:26 +0900

    #36 feat: 텍스트/이벤트 스트림 메시지 컨버터 구현

--- src/main/java/codesquad/fineants/spring/config/WebConfig.java
+++ src/main/java/codesquad/fineants/spring/config/WebConfig.java
@@ -3,11 +3,13 @@ package codesquad.fineants.spring.config;
 import java.util.List;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import codesquad.fineants.domain.oauth.support.AuthPrincipalArgumentResolver;
+import codesquad.fineants.spring.converter.SseEventStreamMessageConverter;
 import codesquad.fineants.spring.intercetpor.LogoutInterceptor;
 import lombok.RequiredArgsConstructor;
 
@@ -26,5 +28,10 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
 		resolvers.add(authPrincipalArgumentResolver);
+	}
+
+	@Override
+	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+		converters.add(new SseEventStreamMessageConverter());
 	}
 }
---
+++ src/main/java/codesquad/fineants/spring/converter/SseEventStreamMessageConverter.java
@@ -0,0 +1,15 @@
+package codesquad.fineants.spring.converter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+
+public class SseEventStreamMessageConverter extends MappingJackson2HttpMessageConverter {
+	public SseEventStreamMessageConverter() {
+		List<MediaType> mediaTypes = new ArrayList<>();
+		mediaTypes.add(new MediaType("text", "event-stream"));
+		setSupportedMediaTypes(mediaTypes);
+	}
+}
