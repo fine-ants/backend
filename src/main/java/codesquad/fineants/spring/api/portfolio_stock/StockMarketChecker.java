921121f39bef9ad0c5dc30d7a2fcdb08043977b1
yonghwankim-dev <dragonbead95@naver.com>
2023-11-29 16:49:34 +0900

    #42 feat: 장시간을 구분하여 SSE 응답하도록 변경

---
+++ src/main/java/codesquad/fineants/spring/api/portfolio_stock/StockMarketChecker.java
@@ -0,0 +1,29 @@
+package codesquad.fineants.spring.api.portfolio_stock;
+
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class StockMarketChecker {
+	private static final LocalTime MARKET_OPEN_TIME = LocalTime.of(9, 0);
+	private static final LocalTime MARKET_CLOSE_TIME = LocalTime.of(15, 30);
+
+	// 주식 시장이 열려 있는지 확인한다
+	public boolean isMarketOpen(LocalDateTime dateTime) {
+		DayOfWeek dayOfWeek = dateTime.getDayOfWeek();
+		LocalTime currentTime = dateTime.toLocalTime();
+
+		if (dayOfWeek == DayOfWeek.SATURDAY || dayOfWeek == DayOfWeek.SUNDAY) {
+			return false;
+		}
+
+		// 평일인 경우 개장 시간과 폐장 시간 사이에 있는지 확인한다
+		if (currentTime.isAfter(MARKET_OPEN_TIME) && currentTime.isBefore(MARKET_CLOSE_TIME)) {
+			return true;
+		}
+		return false;
+	}
+}
