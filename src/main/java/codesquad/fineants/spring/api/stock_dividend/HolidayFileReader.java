8ed6cada43d122af2c3772d4c7d2370102c7d4fd
yein-lee <agop0310@gmail.com>
2023-11-29 16:16:24 +0900

    feat: make ex-dividend date tsv

---
+++ src/main/java/codesquad/fineants/spring/api/stock_dividend/HolidayFileReader.java
@@ -0,0 +1,39 @@
+package codesquad.fineants.spring.api.stock_dividend;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Component;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class HolidayFileReader {
+
+    private static String FILE_NAME = "holidays.tsv";
+
+    public List<HolidayDto> read() throws IOException {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+        Resource resource = new ClassPathResource(FILE_NAME);
+        try (InputStream inputStream = resource.getInputStream();
+             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+
+
+            return reader.lines()
+                    .skip(1)
+                    .map(line -> line.split("\t"))
+                    .map(parts -> new HolidayDto(
+                            LocalDate.parse(parts[0], formatter),
+                            parts[1]))
+                    .collect(Collectors.toList());
+        }
+    }
+}
