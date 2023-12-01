d067b5600b5b5b74020f53119d042377d3999760
YongHwan Kim <dragonbead95@naver.com>
2023-12-01 17:36:53 +0900

    [feat] 포트폴리오 상세 조회 및 종목 조회시 SSE 로직 변경 (#47)
    * #42 fix: 배당금 응답 내역 삭제
    
    * #42 test: 배당금 관련 단언문 삭제
    
    * #42 test: 샘플 데이터 생성을 메소드로 분리
    
    * #42 test: 포트폴리오 등록번호 수정
    
    * #42 feat: 장시간을 구분하여 SSE 응답하도록 변경
    
    * #42 test: 테스트 코드에 StockMarketCheker 목빈 객체 추가
    
    * #42 feat: emitter onTimeout 설정
    
    * #42 feat: complete 메시지 추가
    
    - 장시간이 아닌 시간에는 한번의 sse 응답을 수행하고 complete 메시지를 응답하여 응답이 완료되었다는 메시지를 날리고 Http 연결을 종료한다
    
    * #42 test: 테스트 코드 수정
    
    * #42 fix: dividned, dividendMonth 순서 변경 및 빌더 수정
    
    * #42 fix: 로직 오타 수정
    
    * #42 fix: 로직 오타 수정
    
    * #42 fix: sse timeout 시간 변경

--- src/main/java/codesquad/fineants/domain/stock_dividend/StockDividend.java
+++ src/main/java/codesquad/fineants/domain/stock_dividend/StockDividend.java
@@ -3,7 +3,16 @@ package codesquad.fineants.domain.stock_dividend;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 import codesquad.fineants.domain.BaseEntity;
 import codesquad.fineants.domain.stock.Stock;
@@ -15,30 +24,34 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "stock_dividend", uniqueConstraints = {
-		@UniqueConstraint(columnNames = {"ticker_symbol", "recordDate"})
+	@UniqueConstraint(columnNames = {"ticker_symbol", "recordDate"})
 })
 @Entity
 public class StockDividend extends BaseEntity {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private LocalDateTime dividendMonth;
+	private Long dividend;
 	@Column(nullable = false)
 	private LocalDate exDividendDate;
 	@Column(nullable = false)
 	private LocalDate recordDate;
 	@Column(nullable = true)
 	private LocalDate paymentDate;
-	private Long dividend;
+	private LocalDateTime dividendMonth;
 
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "ticker_symbol", referencedColumnName = "tickerSymbol")
 	private Stock stock;
 
 	@Builder
-	public StockDividend(Long id, LocalDateTime dividendMonth, Long dividend, Stock stock) {
+	public StockDividend(Long id, LocalDateTime dividendMonth, LocalDate exDividendDate, LocalDate recordDate,
+		LocalDate paymentDate, Long dividend, Stock stock) {
 		this.id = id;
 		this.dividendMonth = dividendMonth;
+		this.exDividendDate = exDividendDate;
+		this.recordDate = recordDate;
+		this.paymentDate = paymentDate;
 		this.dividend = dividend;
 		this.stock = stock;
 	}
--- src/test/java/codesquad/fineants/spring/api/portfolio_stock/PortfolioStockServiceTest.java
+++ src/test/java/codesquad/fineants/spring/api/portfolio_stock/PortfolioStockServiceTest.java
@@ -115,22 +115,30 @@ class PortfolioStockServiceTest {
 
 		StockDividend stockDividend1Q = StockDividend.builder()
 			.dividend(361L)
-			.dividendMonth(LocalDate.of(2023, 4, 1).atStartOfDay())
+			.exDividendDate(LocalDate.of(2022, 12, 30))
+			.recordDate(LocalDate.of(2022, 12, 31))
+			.paymentDate(LocalDate.of(2023, 4, 14))
 			.stock(this.stock)
 			.build();
 		StockDividend stockDividend2Q = StockDividend.builder()
 			.dividend(361L)
-			.dividendMonth(LocalDate.of(2023, 5, 1).atStartOfDay())
+			.exDividendDate(LocalDate.of(2023, 3, 30))
+			.recordDate(LocalDate.of(2023, 3, 31))
+			.paymentDate(LocalDate.of(2023, 5, 17))
 			.stock(this.stock)
 			.build();
 		StockDividend stockDividend3Q = StockDividend.builder()
 			.dividend(361L)
-			.dividendMonth(LocalDate.of(2023, 8, 1).atStartOfDay())
+			.exDividendDate(LocalDate.of(2023, 6, 29))
+			.recordDate(LocalDate.of(2023, 6, 30))
+			.paymentDate(LocalDate.of(2023, 8, 16))
 			.stock(this.stock)
 			.build();
 		StockDividend stockDividend4Q = StockDividend.builder()
 			.dividend(361L)
-			.dividendMonth(LocalDate.of(2023, 11, 1).atStartOfDay())
+			.exDividendDate(LocalDate.of(2023, 9, 27))
+			.recordDate(LocalDate.of(2023, 9, 30))
+			.paymentDate(LocalDate.of(2023, 11, 20))
 			.stock(this.stock)
 			.build();
 
