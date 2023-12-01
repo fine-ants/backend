464eb6fa38b84ffef65d9471987ba95051178aba
yhpark95 <98851575+yhpark95@users.noreply.github.com>
2023-12-01 14:41:06 +0900

    #45 feat: jwt refresh token을 redis에 저장하지 않도록 변경

--- src/main/java/codesquad/fineants/spring/api/member/service/OauthMemberRedisService.java
+++ src/main/java/codesquad/fineants/spring/api/member/service/OauthMemberRedisService.java
@@ -1,18 +1,11 @@
 package codesquad.fineants.spring.api.member.service;
 
-import java.util.Objects;
-import java.util.Set;
 import java.util.concurrent.TimeUnit;
-import java.util.regex.Pattern;
 
-import org.apache.logging.log4j.util.Strings;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
-import codesquad.fineants.domain.jwt.Jwt;
-import codesquad.fineants.spring.api.errors.errorcode.JwtErrorCode;
 import codesquad.fineants.spring.api.errors.errorcode.OauthErrorCode;
-import codesquad.fineants.spring.api.errors.exception.BadRequestException;
 import codesquad.fineants.spring.api.errors.exception.UnAuthorizationException;
 import lombok.RequiredArgsConstructor;
 
@@ -21,8 +14,6 @@ import lombok.RequiredArgsConstructor;
 public class OauthMemberRedisService {
 
 	private static final String LOGOUT = "logout";
-	private static final String REFRESH_TOKEN_PREFIX = "RT:";
-	private static final Pattern REFRESH_TOKEN_PATTERN = Pattern.compile("RT:*");
 
 	private final RedisTemplate<String, String> redisTemplate;
 
@@ -30,34 +21,8 @@ public class OauthMemberRedisService {
 		return redisTemplate.opsForValue().get(key);
 	}
 
-	public String findEmailBy(String refreshToken) {
-		Set<String> keys = redisTemplate.keys(REFRESH_TOKEN_PATTERN.pattern());
-		if (keys == null) {
-			throw new UnAuthorizationException(JwtErrorCode.EMPTY_TOKEN);
-		}
-		return keys.stream()
-			.filter(key -> Objects.equals(redisTemplate.opsForValue().get(key), refreshToken))
-			.findAny()
-			.map(email -> email.replace(REFRESH_TOKEN_PREFIX, Strings.EMPTY))
-			.orElseThrow(() -> new BadRequestException(JwtErrorCode.INVALID_TOKEN));
-	}
-
-	public void saveRefreshToken(String key, Jwt jwt) {
-		redisTemplate.opsForValue().set(key,
-			jwt.getRefreshToken(),
-			jwt.convertExpireDateRefreshTokenTimeWithLong(),
-			TimeUnit.MILLISECONDS);
-	}
-
-	public boolean deleteRefreshToken(String key) {
-		if (key == null) {
-			return false;
-		}
-		return Boolean.TRUE.equals(redisTemplate.delete(key));
-	}
-
-	public void banAccessToken(String accessToken, long expiration) {
-		redisTemplate.opsForValue().set(accessToken, LOGOUT, expiration, TimeUnit.MILLISECONDS);
+	public void banToken(String token, long expiration) {
+		redisTemplate.opsForValue().set(token, LOGOUT, expiration, TimeUnit.MILLISECONDS);
 	}
 
 	public void validateAlreadyLogout(String token) {
