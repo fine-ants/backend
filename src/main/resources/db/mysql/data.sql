6cdbf63ecba2a862a61461ddaf4cc14bfdf7f157
yonghwankim-dev <dragonbead95@naver.com>
2023-12-02 20:58:47 +0900

    #33 feat: sector 데이터 초기화 스크립트 추가

--- src/main/resources/db/mysql/data.sql
+++ src/main/resources/db/mysql/data.sql
@@ -22,6 +22,34 @@ LOAD DATA LOCAL INFILE 'src/main/resources/ex-dividend-date.tsv'
         stock_dividend.dividend = @dividend_per_share,
         stock_dividend.create_at = now();
 
+CREATE TEMPORARY TABLE temp_update_table
+select ticker_symbol, sector
+from stock
+limit 0;
+
+LOAD DATA LOCAL INFILE 'src/main/resources/sectors_kospi.tsv'
+    INTO TABLE temp_update_table
+    FIELDS TERMINATED BY '\t'
+    IGNORE 1 ROWS
+    (@종목코드, @종목명, @시장구분, @업종명, @종가, @대비, @등락률, @시가총액)
+    set ticker_symbol = @종목코드,
+        sector = @업종명;
+
+LOAD DATA LOCAL INFILE 'src/main/resources/sectors_kosdaq.tsv'
+    INTO TABLE temp_update_table
+    FIELDS TERMINATED BY '\t'
+    IGNORE 1 ROWS
+    (@종목코드, @종목명, @시장구분, @업종명, @종가, @대비, @등락률, @시가총액)
+    set ticker_symbol = @종목코드,
+        sector = @업종명;
+
+UPDATE stock
+    INNER JOIN temp_update_table on temp_update_table.ticker_symbol = stock.ticker_symbol
+SET stock.sector = temp_update_table.sector;
+
+DROP TEMPORARY TABLE temp_update_table;
+
+
 INSERT INTO member(create_at, email, nickname, profile_url, provider)
 VALUES (now(), 'dragonbead95@naver.com', '일개미2aa1c3d7',
         'http://k.kakaocdn.net/dn/dpk9l1/btqmGhA2lKL/Oz0wDuJn1YV2DIn92f6DVK/img_640x640.jpg', 'naver');
