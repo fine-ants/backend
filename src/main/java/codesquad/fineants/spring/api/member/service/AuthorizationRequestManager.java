c249051696f966668cc237099326a923b3df0e29
yonghwankim-dev <dragonbead95@naver.com>
2023-12-06 12:22:04 +0900

    #59 feat: AUthorizationRequest 클린 메소드 추가

--- src/main/java/codesquad/fineants/spring/api/member/service/AuthorizationRequestManager.java
+++ src/main/java/codesquad/fineants/spring/api/member/service/AuthorizationRequestManager.java
@@ -1,8 +1,12 @@
 package codesquad.fineants.spring.api.member.service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
 
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import codesquad.fineants.spring.api.errors.errorcode.OauthErrorCode;
@@ -23,5 +27,20 @@ public class AuthorizationRequestManager {
 
 	public void add(String state, AuthorizationRequest request) {
 		store.put(state, request);
+	}
+
+	public int size() {
+		return store.size();
+	}
+
+	@Scheduled(fixedRate = 1L, timeUnit = TimeUnit.MINUTES)
+	public void cleanUpExpirationRequests() {
+		long currentTime = System.currentTimeMillis();
+		List<AuthorizationRequest> requests = new ArrayList<>(store.values());
+		for (AuthorizationRequest request : requests) {
+			if (request.isExpiration(currentTime)) {
+				store.remove(request.getState());
+			}
+		}
 	}
 }
