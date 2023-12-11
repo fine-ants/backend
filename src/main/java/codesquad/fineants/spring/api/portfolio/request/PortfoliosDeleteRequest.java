98c2f27bc3df6f6779836ebc1ba70666a3be92af
Yein Lee (이예인) <39490214+yein-lee@users.noreply.github.com>
2023-12-11 14:08:03 +0900

    [fix] add default constructor (#75)
    * fix: add default constructor
    
    * fix: add default constructor

--- src/main/java/codesquad/fineants/spring/api/portfolio/request/PortfoliosDeleteRequest.java
+++ src/main/java/codesquad/fineants/spring/api/portfolio/request/PortfoliosDeleteRequest.java
@@ -2,9 +2,12 @@ package codesquad.fineants.spring.api.portfolio.request;
 
 import java.util.List;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 @AllArgsConstructor
 @Getter
 public class PortfoliosDeleteRequest {
