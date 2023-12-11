7e8f8fdb497f0959930266aff1f44140a839c751
yein-lee <agop0310@gmail.com>
2023-12-11 18:38:41 +0900

    feat: watchlist entity

---
+++ src/main/java/codesquad/fineants/domain/watch_list/WatchList.java
@@ -0,0 +1,39 @@
+package codesquad.fineants.domain.watch_list;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import codesquad.fineants.domain.BaseEntity;
+import codesquad.fineants.domain.member.Member;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class WatchList extends BaseEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	@Column(name = "id", nullable = false)
+	private Long id;
+
+	private String name;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_id")
+	private Member member;
+
+	@Builder
+	public WatchList(String name, Member member){
+		this.name = name;
+		this.member = member;
+	}
+}
---
+++ src/main/java/codesquad/fineants/domain/watch_stock/WatchStockRepository.java
@@ -0,0 +1,6 @@
+package codesquad.fineants.domain.watch_stock;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WatchStockRepository extends JpaRepository<WatchStock, Long> {
+}
