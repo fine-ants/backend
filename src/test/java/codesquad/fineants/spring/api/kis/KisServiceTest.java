b1add821b216cb7ef4f1b7680a2c8e5d49d2c2c5
yonghwankim-dev <dragonbead95@naver.com>
2023-12-24 14:53:33 +0900

    #90 test: 액세스 토큰 발급 테스트 코드 추가

--- src/test/java/codesquad/fineants/spring/api/kis/KisServiceTest.java
+++ src/test/java/codesquad/fineants/spring/api/kis/KisServiceTest.java
@@ -4,6 +4,10 @@ import static org.assertj.core.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.*;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
@@ -11,6 +15,7 @@ import java.util.concurrent.TimeoutException;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -18,6 +23,7 @@ import org.springframework.test.context.ActiveProfiles;
 
 import codesquad.fineants.spring.api.kis.client.KisClient;
 import codesquad.fineants.spring.api.kis.manager.CurrentPriceManager;
+import codesquad.fineants.spring.api.kis.manager.KisAccessTokenManager;
 import codesquad.fineants.spring.api.kis.response.CurrentPriceResponse;
 import lombok.extern.slf4j.Slf4j;
 
@@ -34,6 +40,12 @@ class KisServiceTest {
 
 	@MockBean
 	private KisClient client;
+
+	@MockBean
+	private KisAccessTokenManager kisAccessTokenManager;
+
+	@Autowired
+	private KisRedisService kisRedisService;
 
 	@DisplayName("주식 현재가 시세를 가져온다")
 	@Test
@@ -78,6 +90,31 @@ class KisServiceTest {
 		// then
 		assertThat(response).extracting("tickerSymbol").isEqualTo(tickerSymbol);
 		assertThat(response).extracting("currentPrice").isNotNull();
+	}
+
+	@DisplayName("현재가 및 종가 갱신 전에 액세스 토큰을 새로 발급받아 갱신한다")
+	@Test
+	void refreshStockPrice() {
+		// given
+		kisRedisService.deleteAccessTokenMap();
+		given(kisAccessTokenManager.isAccessTokenExpired(ArgumentMatchers.any(LocalDateTime.class)))
+			.willReturn(true);
+		given(client.accessToken()).willReturn(createAccessTokenMap(LocalDateTime.of(2023, 12, 24, 15, 0, 0)));
+		// when
+		kisService.refreshStockPrice();
+		// then
+		assertThat(kisRedisService.getAccessTokenMap()).isNotNull();
+	}
+
+	private Map<String, Object> createAccessTokenMap(LocalDateTime now) {
+		Map<String, Object> map = new HashMap<>();
+		map.put("access_token",
+			"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0b2tlbiIsImF1ZCI6IjBiYWFlYzg1LWU0YjctNDFlOS05ODk1LTUyNDE2ODRjNDhkOSIsImlzcyI6InVub2d3IiwiZXhwIjoxNzAzNDcxMzg2LCJpYXQiOjE3MDMzODQ5ODYsImp0aSI6IlBTRGc4WlVJd041eVl5ZkR6bnA0TDM2Z2xhRUpic2RJNGd6biJ9.mrJht_O2aRrhSPN1DSmHKarwAfgDpr4GECvF30Is2EI0W6ypbe7DXwXmluhQXT0h1g7OHhGhyBhDNtya4LcctQ");
+		map.put("access_token_token_expired",
+			now.plusDays(1L).format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+		map.put("token_type", "Bearer");
+		map.put("expires_in", 86400);
+		return map;
 	}
 
 }
