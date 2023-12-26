7be9c9e9c77fd5bf0e073295a51e150ef6e58c4d
yonghwankim-dev <dragonbead95@naver.com>
2023-12-26 23:39:00 +0900

    #90 test: 종목 가격 갱신 테스트 코드 추가

--- src/test/java/codesquad/fineants/spring/api/kis/KisServiceTest.java
+++ src/test/java/codesquad/fineants/spring/api/kis/KisServiceTest.java
@@ -7,22 +7,33 @@ import static org.mockito.BDDMockito.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
+import java.util.stream.Collectors;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentMatchers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 
+import codesquad.fineants.domain.member.Member;
+import codesquad.fineants.domain.member.MemberRepository;
+import codesquad.fineants.domain.portfolio.Portfolio;
+import codesquad.fineants.domain.portfolio.PortfolioRepository;
+import codesquad.fineants.domain.portfolio_holding.PortfolioHolding;
+import codesquad.fineants.domain.portfolio_holding.PortfolioHoldingRepository;
+import codesquad.fineants.domain.stock.Market;
+import codesquad.fineants.domain.stock.Stock;
+import codesquad.fineants.domain.stock.StockRepository;
+import codesquad.fineants.spring.api.errors.exception.KisException;
 import codesquad.fineants.spring.api.kis.client.KisClient;
-import codesquad.fineants.spring.api.kis.manager.CurrentPriceManager;
 import codesquad.fineants.spring.api.kis.manager.KisAccessTokenManager;
 import codesquad.fineants.spring.api.kis.response.CurrentPriceResponse;
 import lombok.extern.slf4j.Slf4j;
@@ -35,9 +46,6 @@ class KisServiceTest {
 	@Autowired
 	private KisService kisService;
 
-	@Autowired
-	private CurrentPriceManager manager;
-
 	@MockBean
 	private KisClient client;
 
@@ -47,13 +55,32 @@ class KisServiceTest {
 	@Autowired
 	private KisRedisService kisRedisService;
 
+	@Autowired
+	private MemberRepository memberRepository;
+
+	@Autowired
+	private PortfolioRepository portfolioRepository;
+
+	@Autowired
+	private PortfolioHoldingRepository portfolioHoldingRepository;
+
+	@Autowired
+	private StockRepository stockRepository;
+
+	@AfterEach
+	void tearDown() {
+		portfolioHoldingRepository.deleteAllInBatch();
+		portfolioRepository.deleteAllInBatch();
+		memberRepository.deleteAllInBatch();
+		stockRepository.deleteAllInBatch();
+	}
+
 	@DisplayName("주식 현재가 시세를 가져온다")
 	@Test
 	void readRealTimeCurrentPrice() {
 		// given
 		String tickerSymbol = "005930";
-		given(kisAccessTokenManager.createAuthorization()).willReturn(
-			"Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0b2tlbiIsImF1ZCI6Ijg5MjBlNjM2LTNkYmItNGU5MS04ZGJmLWJmZDU5ZmI2YjAwYiIsImlzcyI6InVub2d3IiwiZXhwIjoxNzAzNTcwOTA0LCJpYXQiOjE3MDM0ODQ1MDQsImp0aSI6IlBTRGc4WlVJd041eVl5ZkR6bnA0TDM2Z2xhRUpic2RJNGd6biJ9.z8dh9rlOyPq_ukm9KeCz0tkKI2QaHEe07LhXTcKQBrcP1-uiW3dwAwdknpAojJZ7aUWLUaQQn0HmjTCttjSJaA");
+		given(kisAccessTokenManager.createAuthorization()).willReturn(createAuthorization());
 		given(client.readRealTimeCurrentPrice(anyString(), anyString())).willReturn(60000L);
 		// when
 		CurrentPriceResponse response = kisService.readRealTimeCurrentPrice(tickerSymbol);
@@ -98,13 +125,44 @@ class KisServiceTest {
 	void refreshStockPrice() {
 		// given
 		kisRedisService.deleteAccessTokenMap();
-		given(kisAccessTokenManager.isAccessTokenExpired(ArgumentMatchers.any(LocalDateTime.class)))
+		given(kisAccessTokenManager.isAccessTokenExpired(any(LocalDateTime.class)))
 			.willReturn(true);
 		given(client.accessToken()).willReturn(createAccessTokenMap(LocalDateTime.now()));
 		// when
 		kisService.refreshStockPrice();
 		// then
 		assertThat(kisRedisService.getAccessTokenMap()).isNotNull();
+	}
+
+	@DisplayName("초당 거래건수가 초과되어 현재가 및 종가를 갱신할 수 없다")
+	@Test
+	void refreshStockPriceWhenExceedingTransactionPerSecond() {
+		// given
+		Member member = memberRepository.save(createMember());
+		Portfolio portfolio = portfolioRepository.save(createPortfolio(member));
+		List<String> tickerSymbols = List.of("000270", "000660", "000880", "001360", "001500",
+			"003530", "003550", "003800", "005930", "034220",
+			"035420", "035720", "086520", "115390", "194480",
+			"323410");
+		List<Stock> stocks = stockRepository.saveAll(tickerSymbols.stream()
+			.map(this::createStock)
+			.collect(Collectors.toList()));
+		stocks.forEach(stock -> portfolioHoldingRepository.save(createPortfolioHolding(portfolio, stock)));
+
+		given(kisAccessTokenManager.createAuthorization()).willReturn(createAuthorization());
+		given(client.readRealTimeCurrentPrice(anyString(), anyString())).willThrow(
+			new KisException("초당 거래건수를 초과하였습니다."));
+		given(client.readLastDayClosingPrice(anyString(), anyString())).willThrow(
+			new KisException("초당 거래건수를 초과하였습니다."));
+		// when
+		kisService.refreshStockPrice();
+		// then
+		verify(client, times(16)).readRealTimeCurrentPrice(anyString(), anyString());
+		verify(client, times(16)).readLastDayClosingPrice(anyString(), anyString());
+	}
+
+	private static String createAuthorization() {
+		return "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0b2tlbiIsImF1ZCI6Ijg5MjBlNjM2LTNkYmItNGU5MS04ZGJmLWJmZDU5ZmI2YjAwYiIsImlzcyI6InVub2d3IiwiZXhwIjoxNzAzNTcwOTA0LCJpYXQiOjE3MDM0ODQ1MDQsImp0aSI6IlBTRGc4WlVJd041eVl5ZkR6bnA0TDM2Z2xhRUpic2RJNGd6biJ9.z8dh9rlOyPq_ukm9KeCz0tkKI2QaHEe07LhXTcKQBrcP1-uiW3dwAwdknpAojJZ7aUWLUaQQn0HmjTCttjSJaA";
 	}
 
 	private Map<String, Object> createAccessTokenMap(LocalDateTime now) {
@@ -116,6 +174,46 @@ class KisServiceTest {
 		map.put("token_type", "Bearer");
 		map.put("expires_in", 86400);
 		return map;
+	}
+
+	private Member createMember() {
+		return Member.builder()
+			.nickname("일개미1234")
+			.email("kim1234@gmail.com")
+			.password("kim1234@")
+			.provider("local")
+			.build();
+	}
+
+	private Portfolio createPortfolio(Member member) {
+		return Portfolio.builder()
+			.name("내꿈은 워렌버핏")
+			.securitiesFirm("토스")
+			.budget(1000000L)
+			.targetGain(1500000L)
+			.maximumLoss(900000L)
+			.member(member)
+			.targetGainIsActive(false)
+			.maximumIsActive(false)
+			.build();
+	}
+
+	private Stock createStock(String tickerSymbol) {
+		return Stock.builder()
+			.companyName("임시 종목")
+			.tickerSymbol(tickerSymbol)
+			.companyNameEng("temp stock")
+			.stockCode("1234")
+			.sector("임시섹터")
+			.market(Market.KOSPI)
+			.build();
+	}
+
+	private PortfolioHolding createPortfolioHolding(Portfolio portfolio, Stock stock) {
+		return PortfolioHolding.builder()
+			.portfolio(portfolio)
+			.stock(stock)
+			.build();
 	}
 
 }
