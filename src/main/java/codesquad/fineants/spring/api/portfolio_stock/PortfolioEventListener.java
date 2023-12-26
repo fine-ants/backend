80dfeb0c582de5ab3382103191ac665a40b6f42f
yonghwankim-dev <dragonbead95@naver.com>
2023-12-26 23:39:49 +0900

    #90 refactor: PortfolioEventListener 및 SseEmitter 해시맵 객체로 분리

---
+++ src/main/java/codesquad/fineants/spring/api/portfolio_stock/PortfolioEventListener.java
@@ -0,0 +1,45 @@
+package codesquad.fineants.spring.api.portfolio_stock;
+
+import java.io.IOException;
+
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class PortfolioEventListener {
+
+	private static final String EVENT_NAME = "portfolioDetails";
+	private static final String COMPLETE_NAME = "complete";
+
+	private final StockMarketChecker stockMarketChecker;
+	private final SseEmitterManager manager;
+
+	@Async
+	@EventListener
+	public void handleMessage(PortfolioEvent event) {
+		SseEmitter emitter = manager.get(event.getPortfolioId());
+		try {
+			emitter.send(SseEmitter.event()
+				.data(event.getResponse())
+				.name(EVENT_NAME));
+
+			if (!stockMarketChecker.isMarketOpen(event.getEventDateTime())) {
+				Thread.sleep(2000L);
+				emitter.send(SseEmitter.event()
+					.data("sse complete")
+					.name(COMPLETE_NAME));
+				emitter.complete();
+			}
+		} catch (IOException | InterruptedException e) {
+			log.info(e.getMessage(), e);
+			emitter.completeWithError(e);
+		}
+	}
+}
