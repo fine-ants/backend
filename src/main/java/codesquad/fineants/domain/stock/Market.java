b2402d02e60f7391e4e7cd80230e2b584a6952cc
yonghwankim-dev <dragonbead95@naver.com>
2024-01-03 16:41:49 +0900

    #85 feat: MarketConverter 구현
    - Stock.market 열의 데이터중 "KOSDAQ GLOBAL" 데이터와 같이 공백으로 구분된 데이터를 언더바(_)로 변환하는 컨버터를 추가하였습니다.

--- src/main/java/codesquad/fineants/domain/stock/Market.java
+++ src/main/java/codesquad/fineants/domain/stock/Market.java
@@ -1,5 +1,18 @@
 package codesquad.fineants.domain.stock;
 
+import java.util.Arrays;
+
+import codesquad.fineants.spring.api.errors.errorcode.StockErrorCode;
+import codesquad.fineants.spring.api.errors.exception.NotFoundResourceException;
+
 public enum Market {
-	KOSPI, KOSDAQ, KONEX;
+	KOSPI, KOSDAQ, KONEX, KOSDAQ_GLOBAL;
+
+	public static Market ofMarket(String dbData) {
+		return Arrays.stream(values())
+			.filter(market -> market.name().equals(dbData))
+			.findAny()
+			.orElseThrow(() -> new NotFoundResourceException(StockErrorCode.NOT_FOUND_MARKET,
+				String.format("%s 종류는 찾을 수 없습니다", dbData)));
+	}
 }
