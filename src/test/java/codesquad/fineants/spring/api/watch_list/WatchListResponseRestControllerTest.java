88b6702796ba89e3b065b979eaa2ef15511058e8
yein-lee <agop0310@gmail.com>
2024-01-03 19:49:16 +0900

    test: read watchlist

--- src/test/java/codesquad/fineants/spring/api/watch_list/WatchListResponseRestControllerTest.java
+++ src/test/java/codesquad/fineants/spring/api/watch_list/WatchListResponseRestControllerTest.java
@@ -7,8 +7,11 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -32,6 +35,7 @@ import codesquad.fineants.spring.api.errors.handler.GlobalExceptionHandler;
 import codesquad.fineants.spring.api.watch_list.request.CreateWatchListRequest;
 import codesquad.fineants.spring.api.watch_list.request.CreateWatchStockRequest;
 import codesquad.fineants.spring.api.watch_list.response.CreateWatchListResponse;
+import codesquad.fineants.spring.api.watch_list.response.ReadWatchListResponse;
 import codesquad.fineants.spring.api.watch_list.response.ReadWatchListsResponse;
 import codesquad.fineants.spring.config.JpaAuditingConfiguration;
 import codesquad.fineants.spring.config.SpringConfig;
@@ -140,6 +144,48 @@ class WatchListResponseRestControllerTest {
 			.andExpect(jsonPath("data.watchLists[0].name").value("My WatchList 1"))
 			.andExpect(jsonPath("data.watchLists[1].id").value(2L))
 			.andExpect(jsonPath("data.watchLists[1].name").value("My WatchList 2"));
+	}
+
+	@DisplayName("사용자가 watchlist 단일 조회를 한다.")
+	@Test
+	void readWatchList() throws Exception {
+		// given
+		Member member = Member.builder()
+			.id(1L)
+			.nickname("일개미1234")
+			.email("kim1234@gmail.com")
+			.provider("local")
+			.password("kim1234@")
+			.profileUrl("profileValue")
+			.build();
+		AuthMember authMember = AuthMember.from(member);
+		given(authPrincipalArgumentResolver.supportsParameter(any())).willReturn(true);
+		given(authPrincipalArgumentResolver.resolveArgument(any(), any(), any(), any())).willReturn(authMember);
+
+		List<ReadWatchListResponse> response = Arrays.asList(
+			new ReadWatchListResponse(1L, "삼성전자", "005930", 68000,
+				1200, 1.85f, 2.12f, "제조업",
+				LocalDateTime.of(2023, 12, 2, 15, 0, 0)));
+
+
+		given(watchListService.readWatchList(any(AuthMember.class), any(Long.class))).willReturn(response);
+
+		// when & then
+		mockMvc.perform(get("/api/watchlists/1")
+				.contentType(MediaType.APPLICATION_JSON))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("code").value(equalTo(200)))
+			.andExpect(jsonPath("status").value(equalTo("OK")))
+			.andExpect(jsonPath("message").value(equalTo("관심종목 단일 목록 조회가 완료되었습니다")))
+			.andExpect(jsonPath("data[0].id").value(equalTo(1)))
+			.andExpect(jsonPath("data[0].companyName").value(equalTo("삼성전자")))
+			.andExpect(jsonPath("data[0].tickerSymbol").value(equalTo("005930")))
+			.andExpect(jsonPath("data[0].currentPrice").value(equalTo(68000)))
+			.andExpect(jsonPath("data[0].dailyChange").value(equalTo(1200)))
+			.andExpect(jsonPath("data[0].dailyChangeRate").value(equalTo(1.85)))
+			.andExpect(jsonPath("data[0].annualDividendYield").value(equalTo(2.12)))
+			.andExpect(jsonPath("data[0].sector").value(equalTo("제조업")))
+			.andExpect(jsonPath("data[0].dateAdded").value(equalTo("2023-12-02T15:00:00")));
 	}
 
 	@DisplayName("사용자가 watchlist에 종목을 추가한다.")
