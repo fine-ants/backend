daf9d1291e14afc9d542dd80d05dcdb4d211461d
yonghwankim-dev <dragonbead95@naver.com>
2024-01-10 15:52:12 +0900

    #123 test: 한국투자증권 액세스 토큰 재발급 테스트 코드 추가

--- src/test/java/codesquad/fineants/spring/api/kis/client/KisClientTest.java
+++ src/test/java/codesquad/fineants/spring/api/kis/client/KisClientTest.java
@@ -1,77 +1,96 @@
 package codesquad.fineants.spring.api.kis.client;
 
-import static java.nio.charset.StandardCharsets.*;
-import static org.mockito.BDDMockito.*;
-
+import java.io.IOException;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.reactive.function.client.WebClientResponseException;
 
+import codesquad.fineants.spring.api.kis.properties.OauthKisProperties;
+import codesquad.fineants.spring.api.kis.service.KisService;
 import codesquad.fineants.spring.util.ObjectMapperUtil;
 import lombok.extern.slf4j.Slf4j;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
-import reactor.util.retry.Retry;
 
 @Slf4j
 @ActiveProfiles("test")
 @SpringBootTest
 class KisClientTest {
+	public static MockWebServer mockWebServer;
 
 	@Autowired
+	private OauthKisProperties oauthKisProperties;
+
 	private KisClient kisClient;
 
 	@MockBean
-	private WebClient webClient;
+	private KisService kisService; // 스케줄링 메소드 비활성화
+
+	@BeforeAll
+	static void setUp() throws IOException {
+		mockWebServer = new MockWebServer();
+		mockWebServer.start();
+	}
+
+	@AfterAll
+	static void tearDown() throws IOException {
+		mockWebServer.shutdown();
+	}
+
+	@BeforeEach
+	void initialize() {
+		String baseUrl = String.format("http://localhost:%s", mockWebServer.getPort());
+		this.kisClient = new KisClient(
+			oauthKisProperties,
+			WebClient.builder().baseUrl(baseUrl).build());
+	}
 
 	@DisplayName("한국투자증권 서버로부터 액세스 토큰 발급이 한번 실패하는 경우 재발급을 다시 요청한다")
 	@Test
 	void accessToken_whenIssueAccessToken_thenRetryOnAccessTokenFailure() {
 		// given
-		WebClient.RequestBodyUriSpec requestBodyUriSpec = Mockito.mock(WebClient.RequestBodyUriSpec.class);
-		WebClient.RequestHeadersSpec requestHeadersSpec = Mockito.mock(WebClient.RequestHeadersSpec.class);
-		WebClient.ResponseSpec responseSpec = Mockito.mock(WebClient.ResponseSpec.class);
+		KisAccessToken expectedKisAccessToken = createKisAccessToken();
+		mockWebServer.enqueue(new MockResponse().setResponseCode(403)
+			.setBody(ObjectMapperUtil.serialize(createError()))
+			.addHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON));
+		mockWebServer.enqueue(new MockResponse().setResponseCode(200)
+			.setBody(ObjectMapperUtil.serialize(expectedKisAccessToken))
+			.addHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON));
 
-		KisAccessToken accessToken = createKisAccessToken();
-		when(webClient.post()).thenReturn(requestBodyUriSpec);
-		when(requestBodyUriSpec.uri(anyString())).thenReturn(requestBodyUriSpec);
-		when(requestBodyUriSpec.bodyValue(anyMap())).thenReturn(requestHeadersSpec);
-		when(requestHeadersSpec.retrieve()).thenReturn(responseSpec);
+		// when
+		Mono<KisAccessToken> responseMono = this.kisClient.accessToken("/oauth2/tokenP");
 
-		when(responseSpec.onStatus(any(), any()))
-			.thenReturn(responseSpec);
-		when(responseSpec.bodyToMono(KisAccessToken.class))
-			.thenReturn(Mono.error(createError()));
-		when(responseSpec.bodyToMono(KisAccessToken.class)
-			.retryWhen(Retry.fixedDelay(Long.MAX_VALUE, Duration.ofMinutes(1L)))
-		).thenReturn(Mono.just(accessToken));
-
-		// when & then
-		Mono<KisAccessToken> mono = kisClient.accessToken();
-		StepVerifier.create(mono)
-			.expectNext(accessToken)
-			.verifyComplete();
+		// then
+		StepVerifier
+			.withVirtualTime(() -> responseMono)
+			.expectSubscription()
+			.thenAwait(Duration.ofMinutes(1))
+			.expectNextMatches(expectedKisAccessToken::equals)
+			.expectComplete()
+			.verify();
 	}
 
-	private WebClientResponseException createError() {
+	private Map<String, String> createError() {
 		Map<String, String> responseBody = new HashMap<>();
 		responseBody.put("error_description", "접근토큰 발급 잠시 후 다시 시도하세요(1분당 1회)");
 		responseBody.put("error_code", "EGW00133");
-		return new WebClientResponseException("Forbidden", HttpStatus.FORBIDDEN.value(),
-			"Forbidden", null, ObjectMapperUtil.serialize(responseBody).getBytes(UTF_8),
-			UTF_8);
+		return responseBody;
 	}
 
 	private KisAccessToken createKisAccessToken() {
@@ -83,3 +102,4 @@ class KisClientTest {
 		);
 	}
 }
+
