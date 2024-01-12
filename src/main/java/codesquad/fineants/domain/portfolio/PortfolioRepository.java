2baca1fdf0b91ac6578f5ae81d0d34215abf96ad
yonghwankim-dev <dragonbead95@naver.com>
2024-01-12 17:30:21 +0900

    #123 refactor: 포트폴리오 조회시 fetch join으로 변경
    - N+1 문제를 해결하기 위해서 fetch join 사용

--- src/main/java/codesquad/fineants/domain/portfolio/PortfolioRepository.java
+++ src/main/java/codesquad/fineants/domain/portfolio/PortfolioRepository.java
@@ -3,6 +3,8 @@ package codesquad.fineants.domain.portfolio;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import codesquad.fineants.domain.member.Member;
 
@@ -10,7 +12,9 @@ public interface PortfolioRepository extends JpaRepository<Portfolio, Long> {
 
 	boolean existsByNameAndMember(String name, Member member);
 
-	List<Portfolio> findAllByMemberIdOrderByIdDesc(Long memberId);
+	@Query("select p from Portfolio p join fetch p.member where p.member.id = :memberId order by p.id desc")
+	List<Portfolio> findAllByMemberIdOrderByIdDesc(@Param("memberId") Long memberId);
 
-	List<Portfolio> findAllByMemberId(Long memberId);
+	@Query("select p from Portfolio p join fetch p.member where p.member.id = :memberId")
+	List<Portfolio> findAllByMemberId(@Param("memberId") Long memberId);
 }
