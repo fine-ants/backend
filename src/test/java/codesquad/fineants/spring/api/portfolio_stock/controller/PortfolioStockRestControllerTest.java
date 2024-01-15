23f289ea6c8b37b4b46ce470d29c2fcc2101e2e4
yein-lee <agop0310@gmail.com>
2024-01-15 16:12:27 +0900

    test: #102 purchaseHistory validation

--- src/test/java/codesquad/fineants/spring/api/portfolio_stock/controller/PortfolioStockRestControllerTest.java
+++ src/test/java/codesquad/fineants/spring/api/portfolio_stock/controller/PortfolioStockRestControllerTest.java
@@ -220,6 +220,35 @@ class PortfolioStockRestControllerTest {
 			.andExpect(jsonPath("data").value(equalTo(null)));
 	}
 
+	@DisplayName("사용자는 포트폴리오에 종목만 추가한다")
+	@Test
+	void addPortfolioStockOnly() throws Exception {
+		Member member = createMember();
+		Portfolio portfolio = createPortfolio(member);
+		Stock stock = createStock();
+
+		PortfolioStockCreateResponse response = PortfolioStockCreateResponse.from(
+			PortfolioHolding.empty(portfolio, stock));
+		given(portfolioRepository.findById(anyLong())).willReturn(Optional.of(portfolio));
+		given(portfolioStockService.addPortfolioStock(anyLong(), any(PortfolioStockCreateRequest.class),
+			any(AuthMember.class))).willReturn(response);
+
+		Map<String, Object> requestBodyMap = new HashMap<>();
+		requestBodyMap.put("tickerSymbol", "005930");
+
+		String body = ObjectMapperUtil.serialize(requestBodyMap);
+		Long portfolioId = portfolio.getId();
+		// when & then
+		mockMvc.perform(post("/api/portfolio/" + portfolioId + "/holdings")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(body))
+			.andExpect(status().isCreated())
+			.andExpect(jsonPath("code").value(equalTo(201)))
+			.andExpect(jsonPath("status").value(equalTo("Created")))
+			.andExpect(jsonPath("message").value(equalTo("포트폴리오 종목이 추가되었습니다")))
+			.andExpect(jsonPath("data").value(equalTo(null)));
+	}
+
 	@DisplayName("사용자는 포트폴리오에 종목을 추가할때 stockId를 필수로 같이 전송해야 한다")
 	@Test
 	void addPortfolioStockWithStockIdIsNull() throws Exception {
