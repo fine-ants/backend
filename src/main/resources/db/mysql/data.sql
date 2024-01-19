8e233eae8aebbfe8ef7dcef1d714bd546cea5ec0
yonghwankim-dev <dragonbead95@naver.com>
2024-01-19 14:53:06 +0900

    #122 docs: 포트폴리오 샘플 데이터에 create_at 컬럼 데이터 추가

--- src/main/resources/db/mysql/data.sql
+++ src/main/resources/db/mysql/data.sql
@@ -55,8 +55,8 @@ VALUES (now(), 'dragonbead95@naver.com', '일개미2aa1c3d7',
         'http://k.kakaocdn.net/dn/dpk9l1/btqmGhA2lKL/Oz0wDuJn1YV2DIn92f6DVK/img_640x640.jpg', 'naver');
 
 INSERT INTO fineAnts.portfolio (id, budget, maximum_loss, name, securities_firm, target_gain, target_gain_is_active,
-                                maximum_is_active, member_id)
-VALUES (1, 1000000, 900000, '내꿈은 워렌버핏', '토스증권', 1500000, false, false, 1);
+                                maximum_is_active, member_id, create_at)
+VALUES (1, 1000000, 900000, '내꿈은 워렌버핏', '토스증권', 1500000, false, false, 1, now());
 
 INSERT INTO fineAnts.portfolio_holding (id, create_at, modified_at, portfolio_id, ticker_symbol)
 VALUES (1, '2023-10-26 15:25:39.409612', '2023-10-26 15:25:39.409612', 1, '005930');
