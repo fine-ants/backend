b746669f17f0933ccdfcc16215acd811f1e8102c
yonghwankim-dev <dragonbead95@naver.com>
2024-02-02 15:39:06 +0900

    #185 test: sendNotification 서비스 메소드 테스트 코드 추가

--- src/main/java/codesquad/fineants/spring/api/firebase/NotificationMessage.java
+++ src/main/java/codesquad/fineants/spring/api/firebase/NotificationMessage.java
@@ -1,11 +1,16 @@
 package codesquad.fineants.spring.api.firebase;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Getter
-@RequiredArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
 @ToString
 public class NotificationMessage {
 	private String recipientToken;
