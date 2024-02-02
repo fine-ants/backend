b05ba9bd6261345a8cf7d26e7bc9c9f1ee9ea12d
YongHwan Kim <dragonbead95@naver.com>
2024-02-02 17:18:45 +0900

    [feat] FCM 관련 스프링 빈 구현 (#190)
    * #172 chore: firebase 설정 파일 추가
    
    * #185 feat: Firebase 스프링 빈 및 서비스 추가
    
    * #185 feat: 로깅 추가
    
    * #185 test: sendNotification 서비스 메소드 테스트 코드 추가
    
    * #185 fix: FirebaseApp 중복 문제 해결

--- src/main/java/codesquad/fineants/spring/api/firebase/FirebaseMessagingService.java
+++ src/main/java/codesquad/fineants/spring/api/firebase/FirebaseMessagingService.java
@@ -8,7 +8,9 @@ import com.google.firebase.messaging.Message;
 import com.google.firebase.messaging.Notification;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class FirebaseMessagingService {
@@ -32,7 +34,7 @@ public class FirebaseMessagingService {
 			log.info("푸시 알림 전송, messageId={}", messageId);
 			return "Success Sending Notification";
 		} catch (FirebaseMessagingException e) {
-			e.printStackTrace();
+			log.error(e.getMessage(), e);
 			return "Error Sending Notification";
 		}
 	}
