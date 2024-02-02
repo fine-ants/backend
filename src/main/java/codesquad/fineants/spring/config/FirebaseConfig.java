740cd305236ef579d818f0ace3f5b2a1bb73c7a0
yonghwankim-dev <dragonbead95@naver.com>
2024-02-02 15:57:25 +0900

    #185 fix: FirebaseApp 중복 문제 해결

--- src/main/java/codesquad/fineants/spring/config/FirebaseConfig.java
+++ src/main/java/codesquad/fineants/spring/config/FirebaseConfig.java
@@ -1,6 +1,9 @@
 package codesquad.fineants.spring.config;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Objects;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,13 +18,23 @@ import com.google.firebase.messaging.FirebaseMessaging;
 public class FirebaseConfig {
 	@Bean
 	public FirebaseMessaging firebaseMessaging() throws IOException {
-		GoogleCredentials googleCredentials = GoogleCredentials
-			.fromStream(new ClassPathResource("secret/firebase/fineants-firebase-adminsdk.json").getInputStream());
-		FirebaseOptions firebaseOptions = FirebaseOptions.builder()
-			.setCredentials(googleCredentials)
-			.build();
-		FirebaseApp app = FirebaseApp.initializeApp(firebaseOptions, "fineants");
-		return FirebaseMessaging.getInstance(app);
+		InputStream refreshToken = new ClassPathResource("secret/firebase/fineants-firebase-adminsdk.json")
+			.getInputStream();
+		FirebaseApp firebaseApp = null;
+		List<FirebaseApp> firebaseApps = FirebaseApp.getApps();
 
+		if (firebaseApps != null && !firebaseApps.isEmpty()) {
+			for (FirebaseApp app : firebaseApps) {
+				if (app.getName().equals(FirebaseApp.DEFAULT_APP_NAME)) {
+					firebaseApp = app;
+				}
+			}
+		} else {
+			FirebaseOptions options = FirebaseOptions.builder()
+				.setCredentials(GoogleCredentials.fromStream(refreshToken))
+				.build();
+			firebaseApp = FirebaseApp.initializeApp(options);
+		}
+		return FirebaseMessaging.getInstance(Objects.requireNonNull(firebaseApp));
 	}
 }
