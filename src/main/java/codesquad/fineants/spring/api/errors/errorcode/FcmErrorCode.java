a6c9782e1ef6435b5dbc0038702cf84e1983216f
yonghwankim-dev <dragonbead95@naver.com>
2024-02-05 14:36:24 +0900

    #184 feat: FCM 토큰 등록 서비스시 검증 추가

---
+++ src/main/java/codesquad/fineants/spring/api/errors/errorcode/FcmErrorCode.java
@@ -0,0 +1,25 @@
+package codesquad.fineants.spring.api.errors.errorcode;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum FcmErrorCode implements ErrorCode {
+
+	BAD_REQUEST_FCM_TOKEN(HttpStatus.BAD_REQUEST, "유효하지 않은 FCM 토큰입니다");
+
+	private final HttpStatus httpStatus;
+	private final String message;
+
+	@Override
+	public String toString() {
+		return String.format("%s, %s(name=%s, httpStatus=%s, message=%s)", "FCM 에러 코드",
+			this.getClass().getSimpleName(),
+			name(),
+			httpStatus,
+			message);
+	}
+}
--- src/main/java/codesquad/fineants/spring/api/fcm/service/FcmService.java
+++ src/main/java/codesquad/fineants/spring/api/fcm/service/FcmService.java
@@ -3,17 +3,26 @@ package codesquad.fineants.spring.api.fcm.service;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingException;
+import com.google.firebase.messaging.Message;
+import com.google.firebase.messaging.Notification;
+
 import codesquad.fineants.domain.fcm_token.FcmRepository;
 import codesquad.fineants.domain.fcm_token.FcmToken;
 import codesquad.fineants.domain.member.Member;
 import codesquad.fineants.domain.member.MemberRepository;
 import codesquad.fineants.domain.oauth.support.AuthMember;
+import codesquad.fineants.spring.api.errors.errorcode.FcmErrorCode;
 import codesquad.fineants.spring.api.errors.errorcode.MemberErrorCode;
+import codesquad.fineants.spring.api.errors.exception.BadRequestException;
 import codesquad.fineants.spring.api.errors.exception.NotFoundResourceException;
 import codesquad.fineants.spring.api.fcm.request.FcmRegisterRequest;
 import codesquad.fineants.spring.api.fcm.response.FcmRegisterResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
@@ -21,11 +30,25 @@ public class FcmService {
 
 	private final FcmRepository fcmRepository;
 	private final MemberRepository memberRepository;
+	private final FirebaseMessaging firebaseMessaging;
 
 	@Transactional
 	public FcmRegisterResponse registerToken(FcmRegisterRequest request, AuthMember authMember) {
 		Member member = findMember(authMember);
+		Message message = Message.builder()
+			.setToken(request.getFcmToken())
+			.setNotification(Notification.builder().build())
+			.build();
+
+		try {
+			String messageResult = firebaseMessaging.send(message, true);
+			log.info("messageReulst : {}", messageResult);
+		} catch (FirebaseMessagingException e) {
+			log.info(e.getMessage(), e);
+			throw new BadRequestException(FcmErrorCode.BAD_REQUEST_FCM_TOKEN);
+		}
 		FcmToken saveFcmToken = fcmRepository.save(request.toEntity(member));
+
 		return FcmRegisterResponse.from(saveFcmToken);
 	}
 
