f4c64d2f90b9f56af6f8b0b76fea8379a7b7ca3f
yonghwankim-dev <dragonbead95@naver.com>
2024-02-06 17:22:13 +0900

    #187 fix: isDeletd 조건문 추가

--- src/main/java/codesquad/fineants/domain/notification/NotificationRepository.java
+++ src/main/java/codesquad/fineants/domain/notification/NotificationRepository.java
@@ -8,10 +8,10 @@ import org.springframework.data.repository.query.Param;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
 
-	@Query("select n from Notification n where n.member.id = :memberId order by n.createAt desc")
+	@Query("select n from Notification n where n.member.id = :memberId and n.isDeleted = false order by n.createAt desc")
 	List<Notification> findAllByMemberId(@Param("memberId") Long memberId);
 
-	@Query("select n from Notification n where n.member.id = :memberId and n.id in (:notificationIds)")
+	@Query("select n from Notification n where n.member.id = :memberId and n.isDeleted = false and n.id in (:notificationIds)")
 	List<Notification> findAllByMemberIdAndIds(
 		@Param("memberId") Long memberId,
 		@Param("notificationIds") List<Long> notificationIds
