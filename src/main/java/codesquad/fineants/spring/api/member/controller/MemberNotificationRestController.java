1709370d5a27a04a56e0c31db24ba863ca47874e
yonghwankim-dev <dragonbead95@naver.com>
2024-02-06 17:15:57 +0900

    #187 feat: 특정 알림 삭제 컨트롤러 메소드 구현

--- src/main/java/codesquad/fineants/spring/api/member/controller/MemberNotificationRestController.java
+++ src/main/java/codesquad/fineants/spring/api/member/controller/MemberNotificationRestController.java
@@ -67,4 +67,16 @@ public class MemberNotificationRestController {
 		log.info("회원 알림 모두 삭제 처리 결과 : memberId={}, 삭제한 알림 등록 번호={}", memberId, deletedNotificationIds);
 		return ApiResponse.success(MemberSuccessCode.OK_DELETED_ALL_NOTIFICATIONS);
 	}
+
+	@DeleteMapping("/notifications/{notificationId}")
+	public ApiResponse<Void> deleteNotification(
+		@PathVariable Long memberId,
+		@PathVariable Long notificationId) {
+		List<Long> deletedNotificationIds = notificationService.deleteAllNotifications(
+			memberId,
+			List.of(notificationId)
+		);
+		log.info("회원 알림 모두 삭제 처리 결과 : memberId={}, 삭제한 알림 등록 번호={}", memberId, deletedNotificationIds);
+		return ApiResponse.success(MemberSuccessCode.OK_DELETD_NOTIFICATION);
+	}
 }
