4072c94472e6a548790d6a997ed1de86ff3846f4
yonghwankim-dev <dragonbead95@naver.com>
2024-02-07 16:23:55 +0900

    #187 fix: 코드 분실 해결

--- src/main/java/codesquad/fineants/spring/api/member/controller/MemberNotificationRestController.java
+++ src/main/java/codesquad/fineants/spring/api/member/controller/MemberNotificationRestController.java
@@ -8,13 +8,17 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import codesquad.fineants.spring.api.member.request.MemberNotificationAllDeleteRequest;
 import codesquad.fineants.spring.api.member.request.MemberNotificationAllReadRequest;
+import codesquad.fineants.spring.api.member.request.MemberNotificationPreferenceRequest;
+import codesquad.fineants.spring.api.member.response.MemberNotificationPreferenceResponse;
 import codesquad.fineants.spring.api.member.response.MemberNotificationResponse;
+import codesquad.fineants.spring.api.member.service.MemberNotificationPreferenceService;
 import codesquad.fineants.spring.api.member.service.MemberNotificationService;
 import codesquad.fineants.spring.api.response.ApiResponse;
 import codesquad.fineants.spring.api.success.code.MemberSuccessCode;
@@ -28,6 +32,7 @@ import lombok.extern.slf4j.Slf4j;
 public class MemberNotificationRestController {
 
 	private final MemberNotificationService notificationService;
+	private final MemberNotificationPreferenceService preferenceService;
 
 	// 회원의 알림 목록 조회
 	@GetMapping("/notifications")
@@ -77,5 +82,15 @@ public class MemberNotificationRestController {
 		);
 		log.info("회원 알림 모두 삭제 처리 결과 : memberId={}, 삭제한 알림 등록 번호={}", memberId, deletedNotificationIds);
 		return ApiResponse.success(MemberSuccessCode.OK_DELETED_NOTIFICATION);
+	}
+
+	@PutMapping("/notification/settings")
+	public ApiResponse<Void> updateNotificationPreference(
+		@PathVariable Long memberId,
+		@Valid @RequestBody MemberNotificationPreferenceRequest request) {
+		MemberNotificationPreferenceResponse response = preferenceService.updateNotificationPreference(memberId,
+			request);
+		log.info("회원 알림 설정 수정 처리 결과 : memberId={}, response={}", memberId, request);
+		return ApiResponse.success(MemberSuccessCode.OK_UPDATE_NOTIFICATION_PREFERENCE);
 	}
 }
