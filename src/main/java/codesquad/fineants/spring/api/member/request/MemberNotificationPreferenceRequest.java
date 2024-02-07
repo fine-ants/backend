55defd789987403b021059ed81f5f80ccd42175b
yonghwankim-dev <dragonbead95@naver.com>
2024-02-07 15:22:57 +0900

    #187 fix: validation 애노테이션 추가

--- src/main/java/codesquad/fineants/spring/api/member/request/MemberNotificationPreferenceRequest.java
+++ src/main/java/codesquad/fineants/spring/api/member/request/MemberNotificationPreferenceRequest.java
@@ -1,5 +1,7 @@
 package codesquad.fineants.spring.api.member.request;
 
+import javax.validation.constraints.NotNull;
+
 import codesquad.fineants.domain.notification_preference.NotificationPreference;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -12,9 +14,13 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Builder
 public class MemberNotificationPreferenceRequest {
+	@NotNull(message = "필수 정보입니다")
 	private Boolean browserNotify;
+	@NotNull(message = "필수 정보입니다")
 	private Boolean targetGainNotify;
+	@NotNull(message = "필수 정보입니다")
 	private Boolean maxLossNotify;
+	@NotNull(message = "필수 정보입니다")
 	private Boolean targetPriceNotify;
 
 	public NotificationPreference toEntity() {
