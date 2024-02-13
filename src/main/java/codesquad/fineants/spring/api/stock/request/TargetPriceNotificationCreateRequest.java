36fbccd2c60db8bab1ac60be79e76437ac7027cf
yonghwankim-dev <dragonbead95@naver.com>
2024-02-13 14:53:53 +0900

    #186 feat: 종목 지정가 알림 추가 컨트롤러 및 서비스 구현

---
+++ src/main/java/codesquad/fineants/spring/api/stock/request/TargetPriceNotificationCreateRequest.java
@@ -0,0 +1,31 @@
+package codesquad.fineants.spring.api.stock.request;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PositiveOrZero;
+
+import codesquad.fineants.domain.member.Member;
+import codesquad.fineants.domain.stock.Stock;
+import codesquad.fineants.domain.stock_target_price.StockTargetPrice;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+public class TargetPriceNotificationCreateRequest {
+	@NotNull(message = "필수 정보입니다")
+	@PositiveOrZero(message = "지정가는 0포함 양수여야 합니다")
+	private Long targetPrice;
+
+	public StockTargetPrice toEntity(Member member, Stock stock) {
+		return StockTargetPrice.builder()
+			.member(member)
+			.stock(stock)
+			.targetPrice(targetPrice)
+			.build();
+	}
+}
