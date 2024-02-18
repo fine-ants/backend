d3343aeb7c68e9ddfc0b64c5b276ee53c7cce456
yonghwankim-dev <dragonbead95@naver.com>
2024-02-18 14:38:43 +0900

    #220 test: 메일 서비스 모킹

--- src/test/java/codesquad/fineants/spring/api/portfolio_notification/MailServiceTest.java
+++ src/test/java/codesquad/fineants/spring/api/portfolio_notification/MailServiceTest.java
@@ -1,9 +1,14 @@
 package codesquad.fineants.spring.api.portfolio_notification;
 
+import static org.mockito.BDDMockito.*;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
@@ -13,16 +18,23 @@ class MailServiceTest {
 	@Autowired
 	private MailService service;
 
+	@MockBean
+	private JavaMailSender mailSender;
+
 	@DisplayName("서버는 이메일을 전송한다")
 	@Test
 	void sendEmail() {
 		// given
+		willDoNothing().given(mailSender).send(any(SimpleMailMessage.class));
+
 		String to = "dragonbead95@naver.com";
 		String subject = "스프링부트 메일 테스트";
 		String body = "스프링부트 메일 테스트 내용입니다.";
+
 		// when
 		service.sendEmail(to, subject, body);
-		// then
 
+		// then
+		verify(mailSender, times(1)).send(any(SimpleMailMessage.class));
 	}
 }
