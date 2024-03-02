8b60e3be2f5d286877c524952916fffb3dff889c
yonghwankim-dev <dragonbead95@naver.com>
2024-03-02 14:17:19 +0900

    #246 rename: 메소드간 순서 변경

--- src/main/java/codesquad/fineants/domain/portfolio_holding/PortfolioHoldingRepository.java
+++ src/main/java/codesquad/fineants/domain/portfolio_holding/PortfolioHoldingRepository.java
@@ -11,26 +11,25 @@ import codesquad.fineants.domain.portfolio.Portfolio;
 
 public interface PortfolioHoldingRepository extends JpaRepository<PortfolioHolding, Long> {
 
-	int deleteAllByPortfolioId(Long portFolioId);
-
 	List<PortfolioHolding> findAllByPortfolio(Portfolio portfolio);
 
 	@Query("select distinct s.tickerSymbol from PortfolioHolding p inner join Stock s on p.stock.tickerSymbol = s.tickerSymbol")
 	List<String> findAllTickerSymbol();
 
-	@Query("DELETE FROM PortfolioHolding p WHERE p.stock.tickerSymbol = :tickerSymbol")
-	void deleteByTickerSymbol(@Param("tickerSymbol") String tickerSymbol);
-
 	@Query("SELECT p FROM PortfolioHolding p WHERE p.stock.tickerSymbol = :tickerSymbol")
 	Optional<PortfolioHolding> findByTickerSymbol(@Param("tickerSymbol") String tickerSymbol);
 
-	int deleteAllByIdIn(List<Long> portfolioHoldingIds);
+	@Query("select p from PortfolioHolding p where p.portfolio.id = :portfolioId and p.stock.tickerSymbol = :tickerSymbol")
+	Optional<PortfolioHolding> findByPortfolioIdAndTickerSymbol(
+		@Param("portfolioId") Long portfolioId,
+		@Param("tickerSymbol") String tickerSymbol);
 
 	@Query("select count(p) > 0 from PortfolioHolding p where p.id = :portfolioHoldingId and p.portfolio.member.id = :memberId")
-	boolean existsByIdAndMemberId(@Param("portfolioHoldingId") Long portfolioHoldingId,
+	boolean existsByIdAndMemberId(
+		@Param("portfolioHoldingId") Long portfolioHoldingId,
 		@Param("memberId") Long memberId);
 
-	@Query("select p from PortfolioHolding p where p.portfolio.id = :portfolioId and p.stock.tickerSymbol = :tickerSymbol")
-	Optional<PortfolioHolding> findByPortfolioIdAndTickerSymbol(@Param("portfolioId") Long portfolioId,
-		@Param("tickerSymbol") String tickerSymbol);
+	int deleteAllByPortfolioId(Long portFolioId);
+
+	int deleteAllByIdIn(List<Long> portfolioHoldingIds);
 }
