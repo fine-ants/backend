587c48e9301da4c6d01eb1931c216a51f607b217
yonghwankim-dev <dragonbead95@naver.com>
2024-03-18 16:38:49 +0900

    #272 fix: 스키마 수정

--- src/main/java/codesquad/fineants/domain/purchase_history/PurchaseHistory.java
+++ src/main/java/codesquad/fineants/domain/purchase_history/PurchaseHistory.java
@@ -8,6 +8,7 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
 import codesquad.fineants.domain.BaseEntity;
@@ -33,6 +34,7 @@ public class PurchaseHistory extends BaseEntity {
 	private String memo;
 
 	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "portfolio_holding_id")
 	private PortfolioHolding portfolioHolding;
 
 	@Builder
