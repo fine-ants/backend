2d8e307ee6a06471451c5233b3e35bfb6495976b
yonghwankim-dev <dragonbead95@naver.com>
2024-04-18 16:08:17 +0900

    feat: 코드 정리

--- src/main/java/codesquad/fineants/spring/api/stock_dividend/service/StockDividendService.java
+++ src/main/java/codesquad/fineants/spring/api/stock_dividend/service/StockDividendService.java
@@ -2,7 +2,6 @@ package codesquad.fineants.spring.api.stock_dividend.service;
 
 import java.time.LocalDate;
 import java.time.temporal.TemporalAdjusters;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
@@ -35,21 +34,20 @@ public class StockDividendService {
 
 	@Transactional
 	public void initStockDividend() {
-		List<Dividend> dividends = amazonS3DividendService.fetchDividend();
 		// 기존 종목 배당금 데이터 삭제
 		stockDividendRepository.deleteAllInBatch();
-		// S3에 저장된 종목 배당금으로 초기화
+
 		Map<String, Stock> stockMap = stockRepository.findAll().stream()
 			.collect(Collectors.toMap(Stock::getTickerSymbol, stock -> stock));
 
-		List<StockDividend> stockDividends = new ArrayList<>();
-		for (Dividend dividend : dividends) {
-			if (dividend.containsBy(stockMap)) {
+		// S3에 저장된 종목 배당금으로 초기화
+		List<StockDividend> stockDividends = amazonS3DividendService.fetchDividend().stream()
+			.filter(dividend -> dividend.containsBy(stockMap))
+			.map(dividend -> {
 				Stock stock = dividend.getStockBy(stockMap);
-				StockDividend stockDividend = dividend.toEntity(stock);
-				stockDividends.add(stockDividend);
-			}
-		}
+				return dividend.toEntity(stock);
+			})
+			.collect(Collectors.toList());
 		List<StockDividend> saveStockDividends = stockDividendRepository.saveAll(stockDividends);
 		log.info("save StockDividends size : {}", saveStockDividends.size());
 	}
@@ -63,31 +61,33 @@ public class StockDividendService {
 	 */
 	@Transactional
 	public void refreshStockDividend(LocalDate now) {
-		// 1. 새로운 배당 일정 추가
-		// 올해 말까지의 배당 일정을 조회
+		// 0. 올해 말까지의 배당 일정을 조회
 		LocalDate to = now.with(TemporalAdjusters.lastDayOfYear());
 		List<KisDividend> kisDividends = kisService.fetchDividendAll(now, to);
 
-		// 기존 데이터와 비교하여 새롭게 추가된 배당 일정 탐색
+		// 1. 새로운 배당 일정 탐색
+		Map<String, Stock> stockMap = getStockMapBy(kisDividends);
+
+		// 3. 새로운 배당 일정 추가
+		addNewStockDividend(kisDividends, stockMap);
+
+		// 4. 현금 지급일 수정
+		updateStockDividendWithPaymentDate(kisDividends, stockMap);
+
+		// 5. 범위를 벗어난 배당 일정을 삭제
+		deleteStockDividendNotInRange(now, stockMap);
+	}
+
+	private Map<String, Stock> getStockMapBy(List<KisDividend> kisDividends) {
 		List<String> tickerSymbols = kisDividends.stream()
 			.map(KisDividend::getTickerSymbol)
 			.collect(Collectors.toList());
-		Map<String, Stock> stockMap = stockRepository.findAllWithDividends(tickerSymbols)
+		return stockRepository.findAllWithDividends(tickerSymbols)
 			.stream()
 			.collect(Collectors.toMap(Stock::getTickerSymbol, stock -> stock));
+	}
 
-		// 추가된 배당 일정 탐색
-		List<StockDividend> addStockDividends = kisDividends.stream()
-			.filter(kisDividend -> kisDividend.containsFrom(stockMap))
-			.filter(kisDividend -> !kisDividend.matchTickerSymbolAndRecordDateFrom(stockMap))
-			.map(kisDividend -> kisDividend.toEntity(kisDividend.getStockBy(stockMap)))
-			.collect(Collectors.toList());
-
-		// 탐색된 데이터들 배당 일정 추가
-		log.info("addStockDividends : {}", addStockDividends);
-		stockDividendRepository.saveAll(addStockDividends);
-
-		// 2. 현금 지급일 수정
+	private void updateStockDividendWithPaymentDate(List<KisDividend> kisDividends, Map<String, Stock> stockMap) {
 		// 현금 지급일을 가지고 있지 않은 배당 일정 조회
 		List<StockDividend> changedStockDividends = kisDividends.stream()
 			.filter(kisDividend -> kisDividend.containsFrom(stockMap))
@@ -95,10 +95,7 @@ public class StockDividendService {
 			.map(kisDividend -> {
 				StockDividend stockDividend = kisDividend.getStockDividendByTickerSymbolAndRecordDateFrom(stockMap)
 					.orElse(null);
-				if (stockDividend == null) {
-					return null;
-				}
-				if (stockDividend.hasPaymentDate()) {
+				if (stockDividend == null || stockDividend.hasPaymentDate()) {
 					return null;
 				}
 				StockDividend changeStockDividend = kisDividend.toEntity(kisDividend.getStockBy(stockMap));
@@ -112,9 +109,30 @@ public class StockDividendService {
 			.collect(Collectors.toList());
 		log.info("changedStockDividends : {}", changedStockDividends);
 		stockDividendRepository.saveAll(changedStockDividends);
+	}
 
-		// 범위를 벗어난 배당 일정을 삭제
-		LocalDate from = now.minusYears(1L).with(TemporalAdjusters.firstDayOfYear());
+	private void addNewStockDividend(List<KisDividend> kisDividends, Map<String, Stock> stockMap) {
+		// 추가된 배당 일정 탐색
+		List<StockDividend> addStockDividends = kisDividends.stream()
+			.filter(kisDividend -> kisDividend.containsFrom(stockMap))
+			.filter(kisDividend -> !kisDividend.matchTickerSymbolAndRecordDateFrom(stockMap))
+			.map(kisDividend -> kisDividend.toEntity(kisDividend.getStockBy(stockMap)))
+			.collect(Collectors.toList());
+
+		// 탐색된 데이터들 배당 일정 추가
+		log.info("addStockDividends : {}", addStockDividends);
+		stockDividendRepository.saveAll(addStockDividends);
+	}
+
+	/**
+	 * 작년 1월 1일부터 올해 12월 31일까지 범위에 없는 배당 일정을 제거
+	 * @param now 올해의 기준일자
+	 * @param stockMap 종목 맵
+	 */
+	private void deleteStockDividendNotInRange(LocalDate now, Map<String, Stock> stockMap) {
+		int lastYear = 1;
+		LocalDate from = now.minusYears(lastYear).with(TemporalAdjusters.firstDayOfYear());
+		LocalDate to = now.with(TemporalAdjusters.lastDayOfYear());
 		List<StockDividend> deleteStockDividends = stockMap.values().stream()
 			.map(stock -> stock.getStockDividendNotInRange(from, to))
 			.flatMap(Collection::stream)
