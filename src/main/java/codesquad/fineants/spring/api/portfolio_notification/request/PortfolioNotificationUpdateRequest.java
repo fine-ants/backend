57830813f540509e16757967292b7134ac3f48b2
yonghwankim-dev <dragonbead95@naver.com>
2024-04-26 14:41:40 +0900

    feat: 포트폴리오 알림 수정 검증문 추가

--- src/main/java/codesquad/fineants/spring/api/portfolio_notification/request/PortfolioNotificationUpdateRequest.java
+++ src/main/java/codesquad/fineants/spring/api/portfolio_notification/request/PortfolioNotificationUpdateRequest.java
@@ -4,13 +4,26 @@ import javax.validation.constraints.NotNull;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 @ToString
 public class PortfolioNotificationUpdateRequest {
 	@JsonProperty("isActive")
 	@NotNull(message = "활성화/비활성 정보는 필수정보입니다.")
 	private Boolean isActive;
+
+	public static PortfolioNotificationUpdateRequest active() {
+		return new PortfolioNotificationUpdateRequest(true);
+	}
+
+	public static PortfolioNotificationUpdateRequest inactive() {
+		return new PortfolioNotificationUpdateRequest(false);
+	}
 }
--- src/test/java/codesquad/fineants/spring/api/portfolio_notification/service/PortfolioNotificationServiceTest.java
+++ src/test/java/codesquad/fineants/spring/api/portfolio_notification/service/PortfolioNotificationServiceTest.java
@@ -1,4 +1,4 @@
-package codesquad.fineants.spring.api.portfolio_notification;
+package codesquad.fineants.spring.api.portfolio_notification.service;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -31,12 +31,13 @@ import codesquad.fineants.domain.stock.Market;
 import codesquad.fineants.domain.stock.Stock;
 import codesquad.fineants.domain.stock.StockRepository;
 import codesquad.fineants.spring.AbstractContainerBaseTest;
+import codesquad.fineants.spring.api.common.errors.errorcode.PortfolioErrorCode;
+import codesquad.fineants.spring.api.common.errors.exception.FineAntsException;
 import codesquad.fineants.spring.api.kis.manager.CurrentPriceManager;
 import codesquad.fineants.spring.api.mail.service.MailService;
 import codesquad.fineants.spring.api.portfolio_notification.manager.MailRedisManager;
 import codesquad.fineants.spring.api.portfolio_notification.request.PortfolioNotificationUpdateRequest;
 import codesquad.fineants.spring.api.portfolio_notification.response.PortfolioNotificationUpdateResponse;
-import codesquad.fineants.spring.api.portfolio_notification.service.PortfolioNotificationService;
 
 class PortfolioNotificationServiceTest extends AbstractContainerBaseTest {
 
@@ -111,6 +112,23 @@ class PortfolioNotificationServiceTest extends AbstractContainerBaseTest {
 		);
 	}
 
+	@DisplayName("사용자는 포트폴리오의 목표수익금액이 0원인 경우 알림을 수정할 수 없다")
+	@Test
+	void updateNotificationTargetGain_whenTargetGainIsZero_thenNotUpdate() {
+		// given
+		Member member = memberRepository.save(createMember());
+		Portfolio portfolio = portfolioRepository.save(createPortfolio(member, 1000000L, 0L, 0L));
+
+		PortfolioNotificationUpdateRequest request = PortfolioNotificationUpdateRequest.active();
+		// when
+		Throwable throwable = catchThrowable(() -> service.updateNotificationTargetGain(request, portfolio.getId()));
+
+		// then
+		assertThat(throwable)
+			.isInstanceOf(FineAntsException.class)
+			.hasMessage(PortfolioErrorCode.TARGET_GAIN_IS_ZERO_WITH_NOTIFY_UPDATE.getMessage());
+	}
+
 	@DisplayName("사용자는 포트폴리오 최대손실금액 알림을 활성화한다")
 	@Test
 	void modifyPortfolioMaximumLossNotification() throws JsonProcessingException {
@@ -139,6 +157,23 @@ class PortfolioNotificationServiceTest extends AbstractContainerBaseTest {
 		);
 	}
 
+	@DisplayName("사용자는 포트폴리오의 최대손실금액이 0원이어서 알림을 수정할 수 없다")
+	@Test
+	void updateNotificationMaximumLoss() {
+		// given
+		Member member = memberRepository.save(createMember());
+		Portfolio portfolio = portfolioRepository.save(createPortfolio(member, 1000000L, 1500000L, 0L));
+
+		PortfolioNotificationUpdateRequest request = PortfolioNotificationUpdateRequest.active();
+		// when
+		Throwable throwable = catchThrowable(() -> service.updateNotificationMaximumLoss(request, portfolio.getId()));
+
+		// then
+		assertThat(throwable)
+			.isInstanceOf(FineAntsException.class)
+			.hasMessage(PortfolioErrorCode.MAX_LOSS_IS_ZERO_WITH_NOTIFY_UPDATE.getMessage());
+	}
+
 	private Member createMember() {
 		return Member.builder()
 			.nickname("일개미1234")
@@ -153,16 +188,17 @@ class PortfolioNotificationServiceTest extends AbstractContainerBaseTest {
 	}
 
 	private Portfolio createPortfolio(Member member, Long budget, Long targetGain, Long maximumLoss) {
-		return Portfolio.builder()
-			.name("내꿈은 워렌버핏")
-			.securitiesFirm("토스")
-			.budget(Money.from(budget))
-			.targetGain(Money.from(targetGain))
-			.maximumLoss(Money.from(maximumLoss))
-			.member(member)
-			.targetGainIsActive(true)
-			.maximumLossIsActive(true)
-			.build();
+		return new Portfolio(
+			null,
+			"내꿈은 워렌버핏",
+			"토스증권",
+			Money.from(budget),
+			Money.from(targetGain),
+			Money.from(maximumLoss),
+			true,
+			true,
+			member
+		);
 	}
 
 	private Stock createStock() {
