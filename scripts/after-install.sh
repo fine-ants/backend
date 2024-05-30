c5eebbcd2497ae1b0cad6b3014bfbcd082172f1f
yonghwankim-dev <dragonbead95@naver.com>
2024-05-30 15:34:59 +0900

    #333 docs: after-install.sh 스크립트 수정

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -2,10 +2,10 @@
 
 if [ "$DEPLOYMENT_GROUP_NAME" == "release" ]
 then
-    cp -R /home/ec2-user/build/temp /home/ec2-user/build/release
+    cp -R /home/ec2-user/build/temp/* /home/ec2-user/build/release
 fi
 
 if [ "$DEPLOYMENT_GROUP_NAME" == "dev" ]
 then
-    cp -R /home/ec2-user/build/temp /home/ec2-user/build/dev
+    cp -R /home/ec2-user/build/temp/* /home/ec2-user/build/dev
 fi
