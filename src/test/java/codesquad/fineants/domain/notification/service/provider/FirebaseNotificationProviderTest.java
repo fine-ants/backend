c5d5dab259431fa62868e5a0ba29af54dadd387b
yonghwankim-dev <dragonbead95@naver.com>
2024-06-19 16:11:27 +0900

    #350 test: 최대 손실율 달성 알림 테스트 추가

--- src/test/java/codesquad/fineants/domain/notification/service/provider/FirebaseNotificationProviderTest.java
+++ src/test/java/codesquad/fineants/domain/notification/service/provider/FirebaseNotificationProviderTest.java
@@ -28,6 +28,7 @@ import codesquad.fineants.domain.kis.repository.CurrentPriceRepository;
 import codesquad.fineants.domain.member.domain.entity.Member;
 import codesquad.fineants.domain.member.repository.MemberRepository;
 import codesquad.fineants.domain.notification.domain.dto.response.SentNotifyMessage;
+import codesquad.fineants.domain.notification.domain.entity.policy.maxloss.MaxLossNotificationPolicy;
 import codesquad.fineants.domain.notification.domain.entity.policy.target_gain.TargetGainNotificationPolicy;
 import codesquad.fineants.domain.portfolio.domain.entity.Portfolio;
 import codesquad.fineants.domain.portfolio.repository.PortfolioRepository;
@@ -65,6 +66,9 @@ class FirebaseNotificationProviderTest extends AbstractContainerBaseTest {
 	@Autowired
 	private TargetGainNotificationPolicy targetGainNotificationPolicy;
 
+	@Autowired
+	private MaxLossNotificationPolicy maxLossNotificationPolicy;
+
 	@MockBean
 	private FirebaseMessagingService firebaseMessagingService;
 
@@ -73,7 +77,7 @@ class FirebaseNotificationProviderTest extends AbstractContainerBaseTest {
 
 	@DisplayName("포트폴리오의 목표수익률 달성 알림을 FCM 방식으로 푸시한다")
 	@Test
-	void sendNotification() {
+	void sendNotification_whenPolicyIsTargetGain_thenNotifyMessage() {
 		// given
 		Member member = memberRepository.save(createMember());
 		Stock samsung = stockRepository.save(createSamsungStock());
@@ -95,11 +99,42 @@ class FirebaseNotificationProviderTest extends AbstractContainerBaseTest {
 			.containsExactly("messageId");
 	}
 
+	@DisplayName("포트폴리오의 최대 손실율 달성 알림을 FCM 방식으로 푸시한다")
+	@Test
+	void sendNotification_whenPolicyIsMaximumLoss_thenNotifyMessage() {
+		// given
+		Member member = memberRepository.save(createMember());
+		Stock samsung = stockRepository.save(createSamsungStock());
+
+		currentPriceRepository.addCurrentPrice(KisCurrentPrice.create(samsung.getTickerSymbol(), 50000L));
+		willDoNothing().given(accessTokenAspect).checkAccessTokenExpiration();
+		PurchaseHistory purchaseHistory = createPurchaseHistory(null, LocalDateTime.now(), Count.from(30),
+			Money.won(100000), "첫구매", null);
+		Portfolio portfolio = createPortfolioSample(member, samsung, purchaseHistory);
+
+		given(firebaseMessagingService.send(ArgumentMatchers.any(Message.class)))
+			.willReturn(Optional.of("messageId"));
+
+		// when
+		List<SentNotifyMessage> messages = provider.sendNotification(List.of(portfolio), maxLossNotificationPolicy);
+
+		// then
+		Assertions.assertThat(messages)
+			.hasSize(1)
+			.extracting(SentNotifyMessage::getMessageId)
+			.containsExactly("messageId");
+	}
+
 	private Portfolio createPortfolioSample(Member member, Stock stock) {
+		PurchaseHistory purchaseHistory = createPurchaseHistory(null, LocalDateTime.now(), Count.from(30),
+			Money.won(100), "첫구매", null);
+		return createPortfolioSample(member, stock, purchaseHistory);
+	}
+
+	private Portfolio createPortfolioSample(Member member, Stock stock, PurchaseHistory purchaseHistory) {
 		Portfolio portfolio = portfolioRepository.save(createPortfolio(member));
 		PortfolioHolding holding = holdingRepository.save(createPortfolioHolding(portfolio, stock));
-		PurchaseHistory history = purchaseHistoryRepository.save(
-			createPurchaseHistory(null, LocalDateTime.now(), Count.from(30), Money.won(100), "첫구매", holding));
+		PurchaseHistory history = purchaseHistoryRepository.save(purchaseHistory);
 		holding.addPurchaseHistory(history);
 		holding.applyCurrentPrice(currentPriceRepository);
 		portfolio.addHolding(holding);
