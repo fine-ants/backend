c947067400fd2a9aa1517630d9c98c253d899da7
YongHwan Kim <dragonbead95@naver.com>
2024-07-22 18:11:40 +0900

    [rename] Rest Docs 수정  (#403)
    * refactor: refactor resource authorized
    
    * refactor: refactoring AuthorizationAspect \n - ResourceId annotation 추가 \n - 테스트 코드 실패 해결 \n - ResourceIdAspect 추가
    
    * refactor: 리소스 id 컬렉션에 대한 검증문 리팩토링
    
    - 서비스 매개변수로 리소스 등록번호 컬렉션 전달시 각각의 리소스에 대하여 소유 권한 여부를 검증하도록 리팩토링
    
    * feat: findPortfolio 메서드에 리소스 소유 권한 검증 애노테이션 추가
    
    * feat: PortfolioHoldingService에 리소스 소유 권한 검증 애노테이션 추가
    
    * feat: 리소스 소유 권한 검증 애노테이션 추가
    
    * refactor: annotation에 AuthorizeService 구현체 이름을 전달하는 방식으로 변경
    
    * test: 테스트 실패 해결
    
    - aop 적용에 따른 예외 케이스의 메시지 변경
    
    * test: 테스트 실패 해결
    
    - serviceClass 타입 오타
    
    * feat: PurchaseHistoryService에 @Authorize 애노테이션 적용
    
    * test: 매입이력 서비스 테스트 실패 해결
    
    * rename: AuthorizeService 이름 변경
    
    * move: AuthorizedService 구현체 이동
    
    * feat: fcm, portfolio 도메인에 @Authorized 애노테이션 추가
    
    * test: 종목 지정가 테스트 실패 해결
    
    * feat: 회원 알림 서비스에 리소스 소유 검증 애노테이션 추가
    
    * feat: 관심 종목 리소스 소유 검증 애노테이션  추가
    
    * move: 종목 지정가와 지정가 알림 분리
    
    * docs: rest docs 목차 정리
    
    * docs: rest docs 목차 정리

--- src/main/java/codesquad/fineants/domain/stock_target_price/service/StockTargetPriceService.java
+++ src/main/java/codesquad/fineants/domain/stock_target_price/service/StockTargetPriceService.java
@@ -1,8 +1,6 @@
 package codesquad.fineants.domain.stock_target_price.service;
 
 import java.util.List;
-import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
 import java.util.stream.Collectors;
 
 import org.springframework.security.access.annotation.Secured;
@@ -18,7 +16,6 @@ import codesquad.fineants.domain.stock.repository.StockRepository;
 import codesquad.fineants.domain.stock_target_price.domain.dto.request.TargetPriceNotificationCreateRequest;
 import codesquad.fineants.domain.stock_target_price.domain.dto.request.TargetPriceNotificationUpdateRequest;
 import codesquad.fineants.domain.stock_target_price.domain.dto.response.TargetPriceNotificationCreateResponse;
-import codesquad.fineants.domain.stock_target_price.domain.dto.response.TargetPriceNotificationDeleteResponse;
 import codesquad.fineants.domain.stock_target_price.domain.dto.response.TargetPriceNotificationSearchItem;
 import codesquad.fineants.domain.stock_target_price.domain.dto.response.TargetPriceNotificationSearchResponse;
 import codesquad.fineants.domain.stock_target_price.domain.dto.response.TargetPriceNotificationSpecificItem;
@@ -30,9 +27,7 @@ import codesquad.fineants.domain.stock_target_price.repository.StockTargetPriceR
 import codesquad.fineants.domain.stock_target_price.repository.TargetPriceNotificationRepository;
 import codesquad.fineants.global.common.authorized.Authorized;
 import codesquad.fineants.global.common.authorized.service.StockTargetPriceAuthorizedService;
-import codesquad.fineants.global.common.authorized.service.TargetPriceNotificationAuthorizedService;
 import codesquad.fineants.global.common.resource.ResourceId;
-import codesquad.fineants.global.common.resource.ResourceIds;
 import codesquad.fineants.global.errors.errorcode.MemberErrorCode;
 import codesquad.fineants.global.errors.errorcode.StockErrorCode;
 import codesquad.fineants.global.errors.exception.BadRequestException;
@@ -44,13 +39,7 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
-public class StockTargetPriceNotificationService {
-
-	private static final Executor executor = Executors.newFixedThreadPool(100, r -> {
-		Thread thread = new Thread(r);
-		thread.setDaemon(true);
-		return thread;
-	});
+public class StockTargetPriceService {
 	private static final int TARGET_PRICE_NOTIFICATION_LIMIT = 5;
 
 	private final StockTargetPriceRepository repository;
@@ -59,9 +48,10 @@ public class StockTargetPriceNotificationService {
 	private final StockRepository stockRepository;
 	private final ClosingPriceRepository manager;
 
+	// 종목 지정가 및 지정가 알림 생성
 	@Transactional
 	@Secured("ROLE_USER")
-	public TargetPriceNotificationCreateResponse createStockTargetPriceNotification(
+	public TargetPriceNotificationCreateResponse createStockTargetPrice(
 		TargetPriceNotificationCreateRequest request,
 		Long memberId
 	) {
@@ -118,90 +108,8 @@ public class StockTargetPriceNotificationService {
 			.orElseThrow(() -> new NotFoundResourceException(StockErrorCode.NOT_FOUND_STOCK));
 	}
 
-	// 종목 지정가 알림 단일 제거
-	@Transactional
-	@Authorized(serviceClass = TargetPriceNotificationAuthorizedService.class)
 	@Secured("ROLE_USER")
-	public TargetPriceNotificationDeleteResponse deleteStockTargetPriceNotification(
-		@ResourceId Long targetPriceNotificationId
-	) {
-		List<Long> targetPriceNotificationIds = List.of(targetPriceNotificationId);
-		// 삭제하고자 하는 종목 지정가가 존재하는지 검증
-		verifyExistTargetPriceById(targetPriceNotificationIds);
-
-		TargetPriceNotification targetPriceNotification = findTargetPriceNotification(targetPriceNotificationId);
-		int deletedTargetPriceNotificationCount = deleteAllTargetPriceNotification(targetPriceNotificationIds);
-
-		// TargetPriceNotification이 한개도 없으면 StockTargetPrice 제거
-		if (targetPriceNotification.getStockTargetPrice().getTargetPriceNotifications().isEmpty()) {
-			repository.deleteById(targetPriceNotification.getStockTargetPrice().getId());
-		}
-
-		log.info("종목 지정가 알림 제거 결과 : targetPriceNotificationIds={}, deletedTargetPriceNotificationCount={}",
-			targetPriceNotificationIds, deletedTargetPriceNotificationCount);
-		return TargetPriceNotificationDeleteResponse.from(targetPriceNotificationIds);
-	}
-
-	private TargetPriceNotification findTargetPriceNotification(Long targetPriceNotificationId) {
-		return targetPriceNotificationRepository.findById(targetPriceNotificationId)
-			.orElseThrow(() -> new NotFoundResourceException(StockErrorCode.NOT_FOUND_TARGET_PRICE));
-	}
-
-	// 종목 지정가 존재 검증
-	private void verifyExistTargetPriceById(List<Long> targetPriceNotificationIds) {
-		if (targetPriceNotificationRepository.findAllById(targetPriceNotificationIds).size()
-			!= targetPriceNotificationIds.size()) {
-			throw new NotFoundResourceException(StockErrorCode.NOT_FOUND_TARGET_PRICE);
-		}
-	}
-
-	// 종목 지정가 단일 제거
-	@Transactional
-	@Authorized(serviceClass = StockTargetPriceAuthorizedService.class)
-	public void deleteStockTargetPrice(@ResourceId Long stockTargetPriceId) {
-		StockTargetPrice stockTargetPrice = repository.findById(stockTargetPriceId)
-			.orElseThrow(() -> new NotFoundResourceException(StockErrorCode.NOT_FOUND_STOCK_TARGET_PRICE));
-		targetPriceNotificationRepository.deleteAllByStockTargetPrices(List.of(stockTargetPrice));
-		repository.deleteById(stockTargetPriceId);
-	}
-
-	// 종목 지정가 알림 다수 제거
-	@Transactional
-	@Authorized(serviceClass = TargetPriceNotificationAuthorizedService.class)
-	@Secured("ROLE_USER")
-	public TargetPriceNotificationDeleteResponse deleteAllStockTargetPriceNotification(
-		@ResourceIds List<Long> targetPriceNotificationIds,
-		String tickerSymbol,
-		Long memberId
-	) {
-		// 존재하지 않는 종목 검증
-		verifyExistStock(tickerSymbol);
-		// 존재하지 않는 종목 지정가가 있는지 검증
-		verifyExistTargetPriceById(targetPriceNotificationIds);
-
-		int deletedCount = deleteAllTargetPriceNotification(targetPriceNotificationIds);
-		int deletedStockTargetPriceCount = deleteStockTargetPrice(tickerSymbol, memberId);
-		log.info("종목 지정가 알림 제거 결과 : ids={}, deletedTargetPriceNotificationCount={}, deletedStockTargetPriceCount = {}",
-			targetPriceNotificationIds, deletedCount, deletedStockTargetPriceCount);
-		return TargetPriceNotificationDeleteResponse.from(targetPriceNotificationIds);
-	}
-
-	private void verifyExistStock(String tickerSymbol) {
-		if (stockRepository.findByTickerSymbol(tickerSymbol).isEmpty()) {
-			throw new NotFoundResourceException(StockErrorCode.NOT_FOUND_STOCK);
-		}
-	}
-
-	private int deleteAllTargetPriceNotification(List<Long> targetPriceNotificationIds) {
-		return targetPriceNotificationRepository.deleteAllByTargetPriceNotificationIds(targetPriceNotificationIds);
-	}
-
-	private int deleteStockTargetPrice(String tickerSymbol, Long memberId) {
-		return repository.deleteByTickerSymbolAndMemberId(tickerSymbol, memberId);
-	}
-
-	@Secured("ROLE_USER")
-	public TargetPriceNotificationSearchResponse searchStockTargetPriceNotification(Long memberId) {
+	public TargetPriceNotificationSearchResponse searchStockTargetPrices(Long memberId) {
 		List<StockTargetPrice> stockTargetPrices = repository.findAllByMemberId(memberId);
 		List<TargetPriceNotificationSearchItem> stocks = stockTargetPrices.stream()
 			.map(stockTargetPrice -> TargetPriceNotificationSearchItem.from(stockTargetPrice, manager))
@@ -209,19 +117,8 @@ public class StockTargetPriceNotificationService {
 		return TargetPriceNotificationSearchResponse.from(stocks);
 	}
 
-	@Transactional
 	@Secured("ROLE_USER")
-	public TargetPriceNotificationUpdateResponse updateStockTargetPriceNotification(
-		TargetPriceNotificationUpdateRequest request, Long memberId) {
-		StockTargetPrice stockTargetPrice = repository.findByTickerSymbolAndMemberId(request.getTickerSymbol(),
-				memberId)
-			.orElseThrow(() -> new NotFoundResourceException(StockErrorCode.NOT_FOUND_STOCK_TARGET_PRICE));
-		stockTargetPrice.changeIsActive(request.getIsActive());
-		return TargetPriceNotificationUpdateResponse.from(stockTargetPrice);
-	}
-
-	@Secured("ROLE_USER")
-	public TargetPriceNotificationSpecifiedSearchResponse searchTargetPriceNotifications(
+	public TargetPriceNotificationSpecifiedSearchResponse searchStockTargetPrice(
 		String tickerSymbol,
 		Long memberId
 	) {
@@ -236,5 +133,26 @@ public class StockTargetPriceNotificationService {
 		);
 		log.info("특정 종목의 지정가 알림들 조회 결과 : response={}", response);
 		return response;
+	}
+
+	@Transactional
+	@Secured("ROLE_USER")
+	public TargetPriceNotificationUpdateResponse updateStockTargetPrice(
+		TargetPriceNotificationUpdateRequest request, Long memberId) {
+		StockTargetPrice stockTargetPrice = repository.findByTickerSymbolAndMemberId(request.getTickerSymbol(),
+				memberId)
+			.orElseThrow(() -> new NotFoundResourceException(StockErrorCode.NOT_FOUND_STOCK_TARGET_PRICE));
+		stockTargetPrice.changeIsActive(request.getIsActive());
+		return TargetPriceNotificationUpdateResponse.from(stockTargetPrice);
+	}
+
+	// 종목 지정가 단일 제거
+	@Transactional
+	@Authorized(serviceClass = StockTargetPriceAuthorizedService.class)
+	public void deleteStockTargetPrice(@ResourceId Long stockTargetPriceId) {
+		StockTargetPrice stockTargetPrice = repository.findById(stockTargetPriceId)
+			.orElseThrow(() -> new NotFoundResourceException(StockErrorCode.NOT_FOUND_STOCK_TARGET_PRICE));
+		targetPriceNotificationRepository.deleteAllByStockTargetPrices(List.of(stockTargetPrice));
+		repository.deleteById(stockTargetPriceId);
 	}
 }
--- src/test/java/codesquad/fineants/domain/stock_target_price/service/StockTargetPriceServiceTest.java
+++ src/test/java/codesquad/fineants/domain/stock_target_price/service/StockTargetPriceServiceTest.java
@@ -4,7 +4,6 @@ import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.assertj.core.groups.Tuple;
 import org.junit.jupiter.api.DisplayName;
@@ -13,19 +12,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import codesquad.fineants.AbstractContainerBaseTest;
 import codesquad.fineants.domain.common.money.Money;
-import codesquad.fineants.domain.fcm.repository.FcmRepository;
 import codesquad.fineants.domain.kis.domain.dto.response.KisClosingPrice;
 import codesquad.fineants.domain.kis.repository.ClosingPriceRepository;
 import codesquad.fineants.domain.member.domain.entity.Member;
 import codesquad.fineants.domain.member.repository.MemberRepository;
-import codesquad.fineants.domain.notification.repository.NotificationRepository;
-import codesquad.fineants.domain.notificationpreference.repository.NotificationPreferenceRepository;
 import codesquad.fineants.domain.stock.domain.entity.Stock;
 import codesquad.fineants.domain.stock.repository.StockRepository;
 import codesquad.fineants.domain.stock_target_price.domain.dto.request.TargetPriceNotificationCreateRequest;
 import codesquad.fineants.domain.stock_target_price.domain.dto.request.TargetPriceNotificationUpdateRequest;
 import codesquad.fineants.domain.stock_target_price.domain.dto.response.TargetPriceNotificationCreateResponse;
-import codesquad.fineants.domain.stock_target_price.domain.dto.response.TargetPriceNotificationDeleteResponse;
 import codesquad.fineants.domain.stock_target_price.domain.dto.response.TargetPriceNotificationSearchResponse;
 import codesquad.fineants.domain.stock_target_price.domain.dto.response.TargetPriceNotificationSpecifiedSearchResponse;
 import codesquad.fineants.domain.stock_target_price.domain.dto.response.TargetPriceNotificationUpdateResponse;
@@ -39,10 +34,10 @@ import codesquad.fineants.global.errors.exception.BadRequestException;
 import codesquad.fineants.global.errors.exception.FineAntsException;
 import codesquad.fineants.global.errors.exception.NotFoundResourceException;
 
-class StockTargetPriceNotificationServiceTest extends AbstractContainerBaseTest {
+class StockTargetPriceServiceTest extends AbstractContainerBaseTest {
 
 	@Autowired
-	private StockTargetPriceNotificationService service;
+	private StockTargetPriceService service;
 
 	@Autowired
 	private MemberRepository memberRepository;
@@ -57,20 +52,11 @@ class StockTargetPriceNotificationServiceTest extends AbstractContainerBaseTest 
 	private TargetPriceNotificationRepository targetPriceNotificationRepository;
 
 	@Autowired
-	private NotificationRepository notificationRepository;
-
-	@Autowired
-	private FcmRepository fcmRepository;
-
-	@Autowired
-	private NotificationPreferenceRepository notificationPreferenceRepository;
-
-	@Autowired
 	private ClosingPriceRepository manager;
 
 	@DisplayName("사용자는 종목 지정가 알림을 추가합니다")
 	@Test
-	void createStockTargetPriceNotification() {
+	void createStockTargetPrice() {
 		// given
 		Member member = memberRepository.save(createMember());
 		Stock stock = stockRepository.save(createSamsungStock());
@@ -80,7 +66,7 @@ class StockTargetPriceNotificationServiceTest extends AbstractContainerBaseTest 
 			.build();
 
 		// when
-		TargetPriceNotificationCreateResponse response = service.createStockTargetPriceNotification(request,
+		TargetPriceNotificationCreateResponse response = service.createStockTargetPrice(request,
 			member.getId());
 
 		// then
@@ -97,7 +83,7 @@ class StockTargetPriceNotificationServiceTest extends AbstractContainerBaseTest 
 
 	@DisplayName("사용자는 한 종목의 지정가 알림 개수를 5개를 초과할 수 없다")
 	@Test
-	void createStockTargetPriceNotification_whenTargetPriceNotificationLimit_thenThrow400Error() {
+	void createStockTargetPrice_whenTargetPriceNotificationLimit_thenThrow400Error() {
 		// given
 		Member member = memberRepository.save(createMember());
 		Stock stock = stockRepository.save(createSamsungStock());
@@ -112,7 +98,7 @@ class StockTargetPriceNotificationServiceTest extends AbstractContainerBaseTest 
 
 		// when
 		Throwable throwable = catchThrowable(() ->
-			service.createStockTargetPriceNotification(request, member.getId()));
+			service.createStockTargetPrice(request, member.getId()));
 
 		// then
 		assertThat(throwable)
@@ -122,7 +108,7 @@ class StockTargetPriceNotificationServiceTest extends AbstractContainerBaseTest 
 
 	@DisplayName("사용자는 한 종목의 지정가가 이미 존재하는 경우 추가할 수 없다")
 	@Test
-	void createStockTargetPriceNotification_whenExistTargetPrice_thenThrow400Error() {
+	void createStockTargetPrice_whenExistTargetPrice_thenThrow400Error() {
 		// given
 		Member member = memberRepository.save(createMember());
 		Stock stock = stockRepository.save(createSamsungStock());
@@ -135,7 +121,7 @@ class StockTargetPriceNotificationServiceTest extends AbstractContainerBaseTest 
 
 		// when
 		Throwable throwable = catchThrowable(() ->
-			service.createStockTargetPriceNotification(request, member.getId()));
+			service.createStockTargetPrice(request, member.getId()));
 
 		// then
 		assertThat(throwable)
@@ -162,7 +148,7 @@ class StockTargetPriceNotificationServiceTest extends AbstractContainerBaseTest 
 		manager.addPrice(KisClosingPrice.create(stock.getTickerSymbol(), 50000L));
 		manager.addPrice(KisClosingPrice.create(stock2.getTickerSymbol(), 50000L));
 		// when
-		TargetPriceNotificationSearchResponse response = service.searchStockTargetPriceNotification(member.getId());
+		TargetPriceNotificationSearchResponse response = service.searchStockTargetPrices(member.getId());
 
 		// then
 		assertAll(
@@ -208,7 +194,7 @@ class StockTargetPriceNotificationServiceTest extends AbstractContainerBaseTest 
 		manager.addPrice(KisClosingPrice.create(stock.getTickerSymbol(), 50000L));
 
 		// when
-		TargetPriceNotificationSpecifiedSearchResponse response = service.searchTargetPriceNotifications(
+		TargetPriceNotificationSpecifiedSearchResponse response = service.searchStockTargetPrice(
 			stock.getTickerSymbol(), member.getId());
 
 		// then
@@ -231,7 +217,7 @@ class StockTargetPriceNotificationServiceTest extends AbstractContainerBaseTest 
 		Stock stock = stockRepository.save(createSamsungStock());
 
 		// when
-		TargetPriceNotificationSpecifiedSearchResponse response = service.searchTargetPriceNotifications(
+		TargetPriceNotificationSpecifiedSearchResponse response = service.searchStockTargetPrice(
 			stock.getTickerSymbol(), member.getId());
 
 		// then
@@ -260,7 +246,7 @@ class StockTargetPriceNotificationServiceTest extends AbstractContainerBaseTest 
 			.build();
 
 		// when
-		TargetPriceNotificationUpdateResponse response = service.updateStockTargetPriceNotification(
+		TargetPriceNotificationUpdateResponse response = service.updateStockTargetPrice(
 			request, member.getId());
 
 		// then
@@ -293,7 +279,7 @@ class StockTargetPriceNotificationServiceTest extends AbstractContainerBaseTest 
 			.build();
 
 		// when
-		Throwable throwable = catchThrowable(() -> service.updateStockTargetPriceNotification(request, member.getId()));
+		Throwable throwable = catchThrowable(() -> service.updateStockTargetPrice(request, member.getId()));
 
 		// then
 		assertThat(throwable)
@@ -334,196 +320,6 @@ class StockTargetPriceNotificationServiceTest extends AbstractContainerBaseTest 
 		setAuthentication(hacker);
 		// when
 		Throwable throwable = catchThrowable(() -> service.deleteStockTargetPrice(stockTargetPrice.getId()));
-		// then
-		assertThat(throwable)
-			.isInstanceOf(FineAntsException.class)
-			.hasMessage(MemberErrorCode.FORBIDDEN_MEMBER.getMessage());
-	}
-
-	@DisplayName("사용자는 종목 지정가 알림을 제거합니다")
-	@Test
-	void deleteStockTargetPriceNotification() {
-		// given
-		Member member = memberRepository.save(createMember());
-		Stock stock = stockRepository.save(createSamsungStock());
-		StockTargetPrice stockTargetPrice = repository.save(createStockTargetPrice(member, stock));
-		TargetPriceNotification targetPriceNotification = targetPriceNotificationRepository.save(
-			createTargetPriceNotification(stockTargetPrice));
-
-		Long id = targetPriceNotification.getId();
-
-		setAuthentication(member);
-		// when
-		TargetPriceNotificationDeleteResponse response = service.deleteStockTargetPriceNotification(id);
-
-		// then
-		assertAll(
-			() -> assertThat(response)
-				.extracting("deletedIds")
-				.asList()
-				.hasSize(1),
-			() -> assertThat(targetPriceNotificationRepository.findById(id).isEmpty()).isTrue(),
-			() -> assertThat(repository.findById(stockTargetPrice.getId()).isEmpty()).isTrue()
-		);
-	}
-
-	@DisplayName("사용자는 존재하지 않는 지정가를 삭제할 수 없습니다")
-	@Test
-	void deleteStockTargetPriceNotification_whenNotExistTargetPriceNotificationIds_thenThrow404Error() {
-		// given
-		Member member = memberRepository.save(createMember());
-		Stock stock = stockRepository.save(createSamsungStock());
-		StockTargetPrice stockTargetPrice = repository.save(createStockTargetPrice(member, stock));
-		targetPriceNotificationRepository.save(createTargetPriceNotification(stockTargetPrice));
-
-		Long id = 9999L;
-
-		setAuthentication(member);
-		// when
-		Throwable throwable = catchThrowable(() -> service.deleteStockTargetPriceNotification(id));
-
-		// then
-		assertThat(throwable)
-			.isInstanceOf(NotFoundResourceException.class)
-			.hasMessage(StockErrorCode.NOT_FOUND_TARGET_PRICE.getMessage());
-	}
-
-	@DisplayName("사용자는 다른 사용자의 지정가 알림을 삭제할 수 없습니다.")
-	@Test
-	void deleteStockTargetPriceNotification_whenForbiddenTargetPriceNotificationIds_thenThrow403Error() {
-		// given
-		Member member = memberRepository.save(createMember());
-		Stock stock = stockRepository.save(createSamsungStock());
-		StockTargetPrice stockTargetPrice = repository.save(createStockTargetPrice(member, stock));
-		TargetPriceNotification targetPriceNotification = targetPriceNotificationRepository.save(
-			createTargetPriceNotification(stockTargetPrice));
-
-		Member hacker = memberRepository.save(createMember("일개미4567", "kim2@gmail.com"));
-
-		setAuthentication(hacker);
-		// when
-		Throwable throwable = catchThrowable(
-			() -> service.deleteStockTargetPriceNotification(targetPriceNotification.getId()));
-
-		// then
-		assertThat(throwable)
-			.isInstanceOf(FineAntsException.class)
-			.hasMessage(MemberErrorCode.FORBIDDEN_MEMBER.getMessage());
-	}
-
-	@DisplayName("사용자는 종목 지정가 알림 전체를 삭제합니다")
-	@Test
-	void deleteAllStockTargetPriceNotification() {
-		// given
-		Member member = memberRepository.save(createMember());
-		Stock stock = stockRepository.save(createSamsungStock());
-		StockTargetPrice stockTargetPrice = repository.save(createStockTargetPrice(member, stock));
-		List<TargetPriceNotification> targetPriceNotifications = targetPriceNotificationRepository.saveAll(
-			createTargetPriceNotification(stockTargetPrice, List.of(60000L, 70000L)));
-
-		List<Long> ids = targetPriceNotifications.stream()
-			.map(TargetPriceNotification::getId)
-			.collect(Collectors.toList());
-
-		setAuthentication(member);
-		// when
-		TargetPriceNotificationDeleteResponse response = service.deleteAllStockTargetPriceNotification(
-			ids,
-			stock.getTickerSymbol(),
-			member.getId()
-		);
-
-		// then
-		assertAll(
-			() -> assertThat(response)
-				.extracting("deletedIds")
-				.asList()
-				.hasSize(2),
-			() -> assertThat(targetPriceNotificationRepository.findAllById(ids).isEmpty()).isTrue(),
-			() -> assertThat(repository.findById(stockTargetPrice.getId()).isEmpty()).isTrue()
-		);
-	}
-
-	@DisplayName("사용자는 종목 지정가 알림을 전체 삭제할 때, 존재하지 않는 지정가 알림을 제거할 수 없습니다.")
-	@Test
-	void deleteAllStockTargetPriceNotification_whenNotExistTargetPriceNotificationIds_thenThrow404Error() {
-		// given
-		Member member = memberRepository.save(createMember());
-		Stock stock = stockRepository.save(createSamsungStock());
-		StockTargetPrice stockTargetPrice = repository.save(createStockTargetPrice(member, stock));
-		List<TargetPriceNotification> targetPriceNotifications = targetPriceNotificationRepository.saveAll(
-			createTargetPriceNotification(stockTargetPrice, List.of(60000L, 70000L)));
-
-		List<Long> ids = targetPriceNotifications.stream()
-			.map(TargetPriceNotification::getId)
-			.collect(Collectors.toList());
-		ids.add(9999L);
-
-		setAuthentication(member);
-		// when
-		Throwable throwable = catchThrowable(() -> service.deleteAllStockTargetPriceNotification(
-			ids,
-			stock.getTickerSymbol(),
-			member.getId()
-		));
-
-		// then
-		assertThat(throwable)
-			.isInstanceOf(NotFoundResourceException.class)
-			.hasMessage(StockErrorCode.NOT_FOUND_TARGET_PRICE.getMessage());
-	}
-
-	@DisplayName("사용자는 종목 지정가 알림을 전체 삭제할 때, 존재하지 않는 종목에 대해서 제거할 수 없습니다.")
-	@Test
-	void deleteAllStockTargetPriceNotification_whenNotExistStock_thenThrow404Error() {
-		// given
-		Member member = memberRepository.save(createMember());
-		Stock stock = stockRepository.save(createSamsungStock());
-		StockTargetPrice stockTargetPrice = repository.save(createStockTargetPrice(member, stock));
-		List<TargetPriceNotification> targetPriceNotifications = targetPriceNotificationRepository.saveAll(
-			createTargetPriceNotification(stockTargetPrice, List.of(60000L, 70000L)));
-
-		List<Long> ids = targetPriceNotifications.stream()
-			.map(TargetPriceNotification::getId)
-			.collect(Collectors.toList());
-
-		setAuthentication(member);
-		// when
-		Throwable throwable = catchThrowable(() -> service.deleteAllStockTargetPriceNotification(
-			ids,
-			"999999",
-			member.getId()
-		));
-
-		// then
-		assertThat(throwable)
-			.isInstanceOf(NotFoundResourceException.class)
-			.hasMessage(StockErrorCode.NOT_FOUND_STOCK.getMessage());
-	}
-
-	@DisplayName("사용자는 종목 지정가 알림을 전체 삭제할 때, 다른 사용자의 지정가 알림을 삭제할 수 없습니다")
-	@Test
-	void deleteAllStockTargetPriceNotification_whenForbiddenTargetPriceNotifications_thenThrow403Error() {
-		// given
-		Member member = memberRepository.save(createMember());
-		Member hacker = memberRepository.save(createMember("hacker"));
-		Stock stock = stockRepository.save(createSamsungStock());
-		StockTargetPrice stockTargetPrice = repository.save(createStockTargetPrice(member, stock));
-		List<TargetPriceNotification> targetPriceNotifications = targetPriceNotificationRepository.saveAll(
-			createTargetPriceNotification(stockTargetPrice, List.of(60000L, 70000L)));
-
-		List<Long> ids = targetPriceNotifications.stream()
-			.map(TargetPriceNotification::getId)
-			.collect(Collectors.toList());
-
-		setAuthentication(hacker);
-		// when
-		Throwable throwable = catchThrowable(() -> service.deleteAllStockTargetPriceNotification(
-			ids,
-			stock.getTickerSymbol(),
-			hacker.getId()
-		));
-
 		// then
 		assertThat(throwable)
 			.isInstanceOf(FineAntsException.class)
