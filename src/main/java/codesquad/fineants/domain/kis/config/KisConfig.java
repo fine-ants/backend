812ce931c64c623d967dde08627ad63fdbf72171
yonghwankim-dev <dragonbead95@naver.com>
2024-08-12 11:27:30 +0900

    refactor: 모의 도메인 webClient 스프링 빈 제거

--- src/main/java/codesquad/fineants/domain/kis/config/KisConfig.java
+++ src/main/java/codesquad/fineants/domain/kis/config/KisConfig.java
@@ -6,21 +6,11 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class KisConfig {
-
-	public static final String baseUrl = "https://openapivts.koreainvestment.com:29443";
-	public static final String realBaseUrl = "https://openapi.koreainvestment.com:9443";
-
-	@Bean(name = "kisWebClient")
-	public WebClient webClient() {
-		return WebClient.builder()
-			.baseUrl(baseUrl)
-			.build();
-	}
-
+	
 	@Bean(name = "realKisWebClient")
 	public WebClient realWebClient() {
 		return WebClient.builder()
-			.baseUrl(realBaseUrl)
+			.baseUrl("https://openapi.koreainvestment.com:9443")
 			.build();
 	}
 }
