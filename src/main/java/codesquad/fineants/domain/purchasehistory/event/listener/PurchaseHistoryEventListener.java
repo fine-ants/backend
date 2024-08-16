d0a449bacd867fc25835119bb0f20f720cfcef38
yonghwankim-dev <dragonbead95@naver.com>
2024-08-16 14:33:55 +0900

    feat: @EventListener를 @TransactionalEventListener로 변경

--- src/main/java/codesquad/fineants/domain/purchasehistory/event/listener/PurchaseHistoryEventListener.java
+++ src/main/java/codesquad/fineants/domain/purchasehistory/event/listener/PurchaseHistoryEventListener.java
@@ -2,9 +2,9 @@ package codesquad.fineants.domain.purchasehistory.event.listener;
 
 import java.util.concurrent.CompletableFuture;
 
-import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 import codesquad.fineants.domain.notification.domain.dto.response.PortfolioNotifyMessagesResponse;
 import codesquad.fineants.domain.notification.service.NotificationService;
@@ -22,7 +22,7 @@ public class PurchaseHistoryEventListener {
 
 	// 매입 이력 이벤트가 발생하면 포트폴리오 목표수익률에 달성하면 푸시 알림
 	@Async
-	@EventListener
+	@TransactionalEventListener
 	public CompletableFuture<PortfolioNotifyMessagesResponse> notifyTargetGainBy(PushNotificationEvent event) {
 		PurchaseHistoryEventSendableParameter parameter = event.getValue();
 		return CompletableFuture.supplyAsync(() ->
@@ -31,7 +31,7 @@ public class PurchaseHistoryEventListener {
 
 	// 매입 이력 이벤트가 발생하면 포트폴리오 최대손실율에 도달하면 푸시 알림
 	@Async
-	@EventListener
+	@TransactionalEventListener
 	public CompletableFuture<PortfolioNotifyMessagesResponse> notifyMaxLoss(PushNotificationEvent event) {
 		PurchaseHistoryEventSendableParameter parameter = event.getValue();
 		return CompletableFuture.supplyAsync(() ->
