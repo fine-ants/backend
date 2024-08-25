dc721624a131dfae8b52ac9b2b950a361dc28118
yonghwankim-dev <dragonbead95@naver.com>
2024-08-25 12:07:53 +0900

    feat: CredeintailsTypeException 조건 추가

--- src/main/java/codesquad/fineants/domain/kis/domain/dto/response/KisErrorResponse.java
+++ src/main/java/codesquad/fineants/domain/kis/domain/dto/response/KisErrorResponse.java
@@ -2,6 +2,7 @@ package codesquad.fineants.domain.kis.domain.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import codesquad.fineants.global.errors.exception.kis.CredentialsTypeKisException;
 import codesquad.fineants.global.errors.exception.kis.ExpiredAccessTokenKisException;
 import codesquad.fineants.global.errors.exception.kis.KisException;
 import codesquad.fineants.global.errors.exception.kis.RequestLimitExceededKisException;
@@ -25,6 +26,7 @@ public class KisErrorResponse {
 			case "EGW00201" -> new RequestLimitExceededKisException(returnCode, messageCode, message);
 			case "EGW00133" -> new TokenIssuanceRetryLaterKisException(returnCode, messageCode, message);
 			case "EGW00123" -> new ExpiredAccessTokenKisException(returnCode, messageCode, message);
+			case "EGW00205" -> new CredentialsTypeKisException(returnCode, messageCode, message);
 			default -> new KisException(returnCode, messageCode, message);
 		};
 	}
