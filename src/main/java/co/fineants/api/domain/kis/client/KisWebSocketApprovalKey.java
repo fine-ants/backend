a69c86717b67ce72aab37f6cb4789fd3ea8ab479
yonghwankim-dev <dragonbead95@naver.com>
2024-09-03 15:53:34 +0900

    feat: 정적 팩토리 추가

--- src/main/java/co/fineants/api/domain/kis/client/KisWebSocketApprovalKey.java
+++ src/main/java/co/fineants/api/domain/kis/client/KisWebSocketApprovalKey.java
@@ -3,6 +3,7 @@ package co.fineants.api.domain.kis.client;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,7 +13,12 @@ import lombok.ToString;
 @ToString
 @EqualsAndHashCode
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class KisWebSocketApprovalKey {
 	@JsonProperty("approval_key")
 	private String approvalKey;
+
+	public static KisWebSocketApprovalKey create(String approvalKey) {
+		return new KisWebSocketApprovalKey(approvalKey);
+	}
 }
