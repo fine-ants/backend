e35cf7039440135d9e030ae458c32617431559f7
yonghwankim-dev <dragonbead95@naver.com>
2024-09-03 15:54:04 +0900

    test: 테스트 검증문 수정

--- src/test/java/co/fineants/api/domain/kis/service/KisWebSocketServiceTest.java
+++ src/test/java/co/fineants/api/domain/kis/service/KisWebSocketServiceTest.java
@@ -1,25 +1,41 @@
 package co.fineants.api.domain.kis.service;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import co.fineants.api.AbstractContainerBaseTest;
+import co.fineants.api.domain.kis.client.KisClient;
+import co.fineants.api.domain.kis.client.KisWebSocketApprovalKey;
+import co.fineants.api.domain.kis.repository.CurrentPriceRedisRepository;
+import reactor.core.publisher.Mono;
 
 class KisWebSocketServiceTest extends AbstractContainerBaseTest {
 
 	@Autowired
 	private KisWebSocketService kisWebSocketService;
 
+	@Autowired
+	private CurrentPriceRedisRepository currentPriceRedisRepository;
+
+	@MockBean
+	private KisClient kisClient;
+
 	@DisplayName("삼성전자의 현재가는 50000원이다")
 	@Test
-	void fetchCurrentPrice() throws InterruptedException {
+	void fetchCurrentPrice() {
 		// given
+		BDDMockito.given(kisClient.fetchWebSocketApprovalKey())
+			.willReturn(Mono.just(KisWebSocketApprovalKey.create("approvalKey")));
+
 		String ticker = "005930";
 		// when
 		kisWebSocketService.fetchCurrentPrice(ticker);
 		// then
-		Thread.sleep(10000);
+		Assertions.assertThat(currentPriceRedisRepository.fetchPriceBy(ticker)).isPresent();
 	}
 
 }
