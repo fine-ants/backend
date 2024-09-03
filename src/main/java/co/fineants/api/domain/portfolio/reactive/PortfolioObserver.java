5e0db8efa84b0e31f2f85708be45edf45331f36e
YongHwan Kim <dragonbead95@naver.com>
2024-09-03 14:20:52 +0900

    fix: sse 버그 해결 (#460)

--- src/main/java/co/fineants/api/domain/portfolio/reactive/PortfolioObserver.java
+++ src/main/java/co/fineants/api/domain/portfolio/reactive/PortfolioObserver.java
@@ -25,7 +25,7 @@ public class PortfolioObserver implements Observer<PortfolioHoldingsRealTimeResp
 
 	@Override
 	public void onSubscribe(@NonNull Disposable d) {
-		throw new UnsupportedOperationException("Not implemented");
+		log.info("subscribe portfolio emitter {}", emitter.toString());
 	}
 
 	@Override
