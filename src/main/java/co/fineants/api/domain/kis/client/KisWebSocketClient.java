dbea2133210a5b70540c4701c31db46954374816
yonghwankim-dev <dragonbead95@naver.com>
2024-09-04 16:04:03 +0900

    feat: 웹소켓 클라이언트에 실시간 종목 체결가 핸들링 메서드 구현

--- src/main/java/co/fineants/api/domain/kis/client/KisWebSocketClient.java
+++ src/main/java/co/fineants/api/domain/kis/client/KisWebSocketClient.java
@@ -4,7 +4,10 @@ import java.net.URI;
 
 import org.springframework.stereotype.Component;
 
+import co.fineants.api.domain.kis.domain.dto.response.KisSubscribeResponse;
 import co.fineants.api.domain.kis.factory.WebSocketContainerProviderFactory;
+import co.fineants.api.domain.kis.repository.CurrentPriceRedisRepository;
+import co.fineants.api.global.util.ObjectMapperUtil;
 import jakarta.websocket.ClientEndpoint;
 import jakarta.websocket.CloseReason;
 import jakarta.websocket.OnClose;
@@ -22,6 +25,7 @@ import lombok.extern.slf4j.Slf4j;
 public class KisWebSocketClient {
 	private Session session;
 	private final WebSocketContainerProviderFactory containerProviderFactory;
+	private final CurrentPriceRedisRepository currentPriceRedisRepository;
 
 	public Session connect(URI endpointURI) {
 		try {
@@ -46,7 +50,33 @@ public class KisWebSocketClient {
 
 	@OnMessage
 	public void onMessage(String message) {
-		log.info("Received Message : {}", message);
+		if (isSubscribeSuccessMessage(message)) {
+			KisSubscribeResponse response = ObjectMapperUtil.deserialize(message, KisSubscribeResponse.class);
+			log.debug("kisSubscribeResponse is {}", response);
+		} else if (isRealTimeSigningPriceMessage(message)) {
+			handleStockTextMessage(message);
+		} else {
+			log.info("Received Message : {}", message);
+		}
+	}
+
+	// 실시간 체결가 메시지인지 여부 확인
+	private boolean isRealTimeSigningPriceMessage(String message) {
+		return message.startsWith("0|H0STCNT0");
+	}
+
+	private boolean isSubscribeSuccessMessage(String message) {
+		return message.startsWith("{") && message.endsWith("}") && message.contains("SUBSCRIBE SUCCESS");
+	}
+
+	private void handleStockTextMessage(String message) {
+		String[] parts = message.split("\\|");
+		String[] values = parts[3].split("\\^");
+		String ticker = values[0];
+		String currentPrice = values[2];
+		KisCurrentPrice kisCurrentPrice = KisCurrentPrice.create(ticker, Long.valueOf(currentPrice));
+		currentPriceRedisRepository.savePrice(kisCurrentPrice);
+		log.debug("save the stock currentPrice, {}", kisCurrentPrice);
 	}
 
 	public void sendMessage(String message) {
--- src/main/java/co/fineants/api/domain/kis/controller/KisWebSocketRestController.java
+++ src/main/java/co/fineants/api/domain/kis/controller/KisWebSocketRestController.java
@@ -1,13 +1,13 @@
 package co.fineants.api.domain.kis.controller;
 
-import java.net.URISyntaxException;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import co.fineants.api.domain.kis.service.KisWebSocketService;
+import co.fineants.api.global.api.ApiResponse;
+import co.fineants.api.global.success.KisSuccessCode;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -17,8 +17,8 @@ public class KisWebSocketRestController {
 	private final KisWebSocketService service;
 
 	@GetMapping("/current-price")
-	public String sendMessage(@RequestParam String ticker) throws URISyntaxException {
+	public ApiResponse<Void> sendMessage(@RequestParam String ticker) {
 		service.fetchCurrentPrice(ticker);
-		return "Message sent to WebSocket server: " + ticker;
+		return ApiResponse.success(KisSuccessCode.OK_SIGNING_PRICE_SEND_MESSAGE);
 	}
 }
