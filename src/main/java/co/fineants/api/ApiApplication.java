d38d0b542126758ec9d0d2f6c2f11e6c6de0082c
yonghwankim-dev <dragonbead95@naver.com>
2024-09-06 14:21:18 +0900

    style: 투두 추가 및 메인 애플리케이션 이름 변경

--- src/main/java/co/fineants/api/ApiApplication.java
+++ src/main/java/co/fineants/api/ApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FineAntsApplication {
+public class ApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FineAntsApplication.class, args);
+		SpringApplication.run(ApiApplication.class, args);
 	}
 
 }
