6e9dcacb9cd74c2dd28f0a8d1c2c422a4d06aea6
yonghwankim-dev <dragonbead95@naver.com>
2024-09-07 12:24:53 +0900

    feat: WebClientConfig global 패키지로 이동

--- src/main/java/co/fineants/api/global/config/WebClientConfig.java
+++ src/main/java/co/fineants/api/global/config/WebClientConfig.java
@@ -1,15 +1,22 @@
-package co.fineants.api.domain.kis.config;
+package co.fineants.api.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
-public class KisConfig {
+public class WebClientConfig {
 	@Bean
-	public WebClient webClient() {
+	public WebClient koreaInvestmentWebClient() {
 		return WebClient.builder()
 			.baseUrl("https://openapi.koreainvestment.com:9443")
+			.build();
+	}
+
+	@Bean
+	public WebClient localhostWebClient() {
+		return WebClient.builder()
+			.baseUrl("http://localhost:8080")
 			.build();
 	}
 }
