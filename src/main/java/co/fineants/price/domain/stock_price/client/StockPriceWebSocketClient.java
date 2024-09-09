e54751a7041cff6c04ab38165d49abccb166f278
yonghwankim-dev <dragonbead95@naver.com>
2024-09-09 15:39:37 +0900

    feat: StockPriceWebSocketClient 구현

---
+++ src/main/java/co/fineants/price/domain/stock_price/client/StockPriceWebSocketClient.java
@@ -0,0 +1,115 @@
+package co.fineants.price.domain.stock_price.client;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ExecutionException;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.client.WebSocketClient;
+import org.springframework.web.socket.client.standard.StandardWebSocketClient;
+
+import co.fineants.api.domain.kis.client.KisClient;
+import co.fineants.api.domain.kis.client.KisWebSocketApprovalKey;
+import co.fineants.api.domain.kis.properties.KisHeader;
+import co.fineants.api.domain.kis.properties.kiscodevalue.imple.CustomerType;
+import co.fineants.api.domain.kis.repository.WebSocketApprovalKeyRedisRepository;
+import co.fineants.api.global.common.delay.DelayManager;
+import co.fineants.api.global.util.ObjectMapperUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Mono;
+import reactor.util.retry.Retry;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class StockPriceWebSocketClient {
+
+	private static final String KIS_WEBSOCKET_CURRENT_PRICE_URI =
+		"ws://ops.koreainvestment.com:21000/websocket/tryitout/H0STCNT0";
+
+	private final StockPriceWebSocketHandler stockPriceWebSocketHandler;
+	private final WebSocketApprovalKeyRedisRepository approvalKeyRepository;
+	private final KisClient kisClient;
+	private final DelayManager delayManager;
+
+	@Value("${kis.websocket.auto-connect:true}")
+	private boolean websocketAutoConnect;
+	private final WebSocketClient webSocketClient = new StandardWebSocketClient();
+	private WebSocketSession session = null;
+
+	@PostConstruct
+	private void init() {
+		if (!websocketAutoConnect) {
+			return;
+		}
+		String approvalKey = approvalKeyRepository.fetchApprovalKey().orElse(null);
+		if (approvalKey == null) {
+			this.fetchApprovalKey().ifPresent(approvalKeyRepository::saveApprovalKey);
+		}
+		connect(KIS_WEBSOCKET_CURRENT_PRICE_URI);
+	}
+
+	private Optional<String> fetchApprovalKey() {
+		return kisClient.fetchWebSocketApprovalKey()
+			.retryWhen(Retry.fixedDelay(5, delayManager.fixedAccessTokenDelay()))
+			.onErrorResume(throwable -> {
+				log.error(throwable.getMessage());
+				return Mono.empty();
+			})
+			.log()
+			.blockOptional(delayManager.timeout())
+			.map(KisWebSocketApprovalKey::getApprovalKey);
+	}
+
+	public void connect(String uri) {
+		try {
+			session = webSocketClient.execute(stockPriceWebSocketHandler, uri).get();
+		} catch (InterruptedException | ExecutionException e) {
+			log.error("StockPriceWebSocketClient fail connection, errorMessage={}", e.getMessage());
+			throw new RuntimeException(e);
+		}
+	}
+
+	public boolean sendMessage(String ticker) {
+		if (session != null && session.isOpen()) {
+			try {
+				session.sendMessage(new TextMessage(createCurrentPrice(ticker)));
+				return true;
+			} catch (IOException e) {
+				log.error("StockPriceWebStockClient fail sendMessage, errorMessage={}", e.getMessage());
+				return false;
+			}
+		} else {
+			log.info("WebSocket session is not open");
+			return false;
+		}
+	}
+
+	private String createCurrentPrice(String ticker) {
+		Map<String, Object> requestMap = new HashMap<>();
+		Map<String, String> headerMap = new HashMap<>();
+		headerMap.put(KisHeader.APPROVAL_KEY.name(), approvalKeyRepository.fetchApprovalKey().orElseThrow());
+		headerMap.put(KisHeader.CUSTOMER_TYPE.getHeaderName(), CustomerType.INDIVIDUAL.getCode());
+		headerMap.put(KisHeader.TR_TYPE.name(), "1");
+		headerMap.put(KisHeader.CONTENT_TYPE.name(), "utf-8");
+
+		Map<String, Object> bodyMap = new HashMap<>();
+		Map<String, String> input = new HashMap<>();
+		input.put("tr_id", "H0STCNT0");
+		input.put("tr_key", ticker);
+		bodyMap.put("input", input);
+
+		requestMap.put("header", headerMap);
+		requestMap.put("body", bodyMap);
+
+		return ObjectMapperUtil.serialize(requestMap);
+	}
+}
---
+++ src/main/java/co/fineants/price/domain/stock_price/client/StockPriceWebSocketHandler.java
@@ -0,0 +1,77 @@
+package co.fineants.price.domain.stock_price.client;
+
+import org.jetbrains.annotations.NotNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.WebSocketMessage;
+import org.springframework.web.socket.WebSocketSession;
+
+import co.fineants.api.domain.kis.client.KisCurrentPrice;
+import co.fineants.api.domain.kis.domain.dto.response.KisSubscribeResponse;
+import co.fineants.api.domain.kis.repository.CurrentPriceRedisRepository;
+import co.fineants.api.global.util.ObjectMapperUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class StockPriceWebSocketHandler implements WebSocketHandler {
+
+	private final CurrentPriceRedisRepository currentPriceRedisRepository;
+
+	@Override
+	public void afterConnectionEstablished(@NotNull WebSocketSession session) {
+		log.debug("StockPriceWebSocket connection established, session={}", session);
+	}
+
+	@Override
+	public void handleMessage(@NotNull WebSocketSession session, WebSocketMessage<?> message) throws Exception {
+		String payload = message.getPayload().toString();
+		if (isSubscribeSuccessMessage(payload)) {
+			KisSubscribeResponse response = ObjectMapperUtil.deserialize(payload,
+				KisSubscribeResponse.class);
+			log.debug("kisSubscribeResponse is {}", response);
+		} else if (isRealTimeSigningPriceMessage(payload)) {
+			handleStockTextMessage(payload);
+		} else {
+			log.info("Received Message : {}", message);
+		}
+	}
+
+	// 실시간 체결가 메시지인지 여부 확인
+	private boolean isRealTimeSigningPriceMessage(String message) {
+		return message.startsWith("0|H0STCNT0");
+	}
+
+	private boolean isSubscribeSuccessMessage(String message) {
+		return message.startsWith("{") && message.endsWith("}") && message.contains("SUBSCRIBE SUCCESS");
+	}
+
+	private void handleStockTextMessage(String message) {
+		String[] parts = message.split("\\|");
+		String[] values = parts[3].split("\\^");
+		String ticker = values[0];
+		String currentPrice = values[2];
+		KisCurrentPrice kisCurrentPrice = KisCurrentPrice.create(ticker, Long.valueOf(currentPrice));
+		currentPriceRedisRepository.savePrice(kisCurrentPrice);
+		log.debug("save the stock currentPrice, {}", kisCurrentPrice);
+	}
+
+	@Override
+	public void handleTransportError(@NotNull WebSocketSession session, @NotNull Throwable exception) throws Exception {
+		log.error("Error with websocket session={}, errorMessage={}", session, exception.getMessage());
+	}
+
+	@Override
+	public void afterConnectionClosed(@NotNull WebSocketSession session, @NotNull CloseStatus closeStatus) throws
+		Exception {
+		log.info("close StockPriceWebSocket session={}, closeStatus={}", session, closeStatus);
+	}
+
+	@Override
+	public boolean supportsPartialMessages() {
+		return false;
+	}
+}
