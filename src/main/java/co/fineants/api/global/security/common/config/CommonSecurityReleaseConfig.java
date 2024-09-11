8ee17425f106042595735ebc07b2a62c2238898f
YongHwan Kim <dragonbead95@naver.com>
2024-09-11 14:11:01 +0900

    feat: cors 프로파일별 설정 클래스 추가 (#464)

---
+++ src/main/java/co/fineants/api/global/security/common/config/CommonSecurityReleaseConfig.java
@@ -0,0 +1,36 @@
+package co.fineants.api.global.security.common.config;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.cors.CorsConfiguration;
+
+@Configuration
+@Profile(value = {"local", "release"})
+public class CommonSecurityReleaseConfig {
+
+	@Bean
+	public CorsConfiguration corsConfiguration() {
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowedOrigins(
+			List.of("https://fineants.co",
+				"https://www.fineants.co",
+				"https://release.fineants.co",
+				"http://localhost:5173",
+				"https://localhost:5173",
+				"http://localhost:3000",
+				"https://localhost:3000",
+				"https://accounts.kakao.com/login",
+				"https://nid.naver.com/oauth2.0/authorize",
+				"https://accounts.google.com/o/oauth2/v2/auth"));
+		config.setAllowedMethods(List.of("GET", "POST", "PUT", "UPDATE", "DELETE", "PATCH", "OPTIONS", "HEAD"));
+		config.setAllowedHeaders(Collections.singletonList("*"));
+		config.addExposedHeader("Set-Cookie");
+		config.setAllowCredentials(true);
+		config.setMaxAge(3600L);
+		return config;
+	}
+}
