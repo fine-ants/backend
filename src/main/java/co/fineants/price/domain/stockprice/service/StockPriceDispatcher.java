0cced90ec025a8f236de75ee5e68bae860f59e90
yonghwankim-dev <dragonbead95@naver.com>
2024-09-22 14:57:35 +0900

    feat: 구독 불가능 종목에 대하여 현재가 조회하는 기능 추가

--- src/main/java/co/fineants/price/domain/stockprice/service/StockPriceDispatcher.java
+++ src/main/java/co/fineants/price/domain/stockprice/service/StockPriceDispatcher.java
@@ -1,16 +1,29 @@
 package co.fineants.price.domain.stockprice.service;
 
+import java.time.Duration;
+
 import org.springframework.stereotype.Component;
 
+import co.fineants.api.domain.kis.repository.CurrentPriceRedisRepository;
+import co.fineants.api.domain.kis.service.KisService;
 import co.fineants.price.domain.stockprice.client.StockPriceWebSocketClient;
 import lombok.RequiredArgsConstructor;
+import reactor.util.retry.Retry;
 
 @Component
 @RequiredArgsConstructor
 public class StockPriceDispatcher {
 	private final StockPriceWebSocketClient client;
+	private final KisService kisService;
+	private final CurrentPriceRedisRepository currentPriceRedisRepository;
 
 	public void dispatch(String ticker) {
 		client.sendSubscribeMessage(ticker);
+	}
+
+	public void dispatchCurrentPrice(String ticker) {
+		kisService.fetchCurrentPrice(ticker)
+			.retryWhen(Retry.fixedDelay(5, Duration.ofSeconds(5)))
+			.subscribe(currentPriceRedisRepository::savePrice);
 	}
 }
--- src/main/java/co/fineants/price/domain/stockprice/service/StockPriceService.java
+++ src/main/java/co/fineants/price/domain/stockprice/service/StockPriceService.java
@@ -14,11 +14,15 @@ public class StockPriceService {
 	private final StockPriceDispatcher dispatcher;
 
 	public void pushStocks(Set<String> tickerSymbols) {
-		tickerSymbols.stream()
-			.filter(repository::canSubscribe)
-			.forEach(ticker -> {
+		for (String ticker : tickerSymbols) {
+			// 구독 가능한 종목들에 대해서 종목 실시간 체결가에 대한 저장 및 구독 요청
+			if (repository.canSubscribe(ticker)) {
 				repository.save(ticker);
 				dispatcher.dispatch(ticker);
-			});
+			} else {
+				// 구독 불가능한 종목들에 대해서 종목 현재가 조회 요청
+				dispatcher.dispatchCurrentPrice(ticker);
+			}
+		}
 	}
 }
