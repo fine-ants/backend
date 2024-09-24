794150b6eeb9dcdab90b78065e888a228ef78c80
yonghwankim-dev <dragonbead95@naver.com>
2024-09-24 16:30:02 +0900

    style: 코드 정리

--- src/main/java/co/fineants/price/domain/stockprice/config/StockPriceConfig.java
+++ src/main/java/co/fineants/price/domain/stockprice/config/StockPriceConfig.java
@@ -2,11 +2,9 @@ package co.fineants.price.domain.stockprice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.web.socket.client.WebSocketClient;
 import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 
-@EnableAspectJAutoProxy
 @Configuration
 public class StockPriceConfig {
 
