e897ea2e58bc7ce20a71ebf88f9b8d0e98564ca4
yonghwankim-dev <dragonbead95@naver.com>
2024-10-02 15:31:00 +0900

    feat: 직렬화 캐시 설정

--- src/main/java/co/fineants/api/global/config/CacheConfig.java
+++ src/main/java/co/fineants/api/global/config/CacheConfig.java
@@ -11,12 +11,21 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import lombok.RequiredArgsConstructor;
+
 @EnableCaching
 @Configuration
+@RequiredArgsConstructor
 public class CacheConfig {
+
+	private final ObjectMapper objectMapper;
+
 	@Bean
 	public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
 		// 기본 캐시 설정 (10분 TTL)
@@ -28,8 +37,13 @@ public class CacheConfig {
 		Map<String, RedisCacheConfiguration> cacheConfigurations = new HashMap<>();
 		cacheConfigurations.put("tickerSymbols",
 			RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofMinutes(5))); // 5 minute TTL
+
+		RedisSerializationContext.SerializationPair<Object> serializer = RedisSerializationContext.SerializationPair.fromSerializer(
+			new Jackson2JsonRedisSerializer<>(objectMapper, Object.class));
 		cacheConfigurations.put("lineChartCache",
-			RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofHours(24))); // 24 hourTTL
+			RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofHours(24))
+				.serializeValuesWith(serializer)
+		); // 24 hourTTL
 
 		return RedisCacheManager.builder(redisConnectionFactory)
 			.cacheDefaults(defaultCacheConfig)
