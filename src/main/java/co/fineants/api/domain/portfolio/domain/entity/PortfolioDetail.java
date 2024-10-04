3b8b335c553a9e4460dd264f3ca6e2242bcf41f6
yonghwankim-dev <dragonbead95@naver.com>
2024-10-04 13:08:14 +0900

    comment: javadoc 수정

--- src/main/java/co/fineants/api/domain/portfolio/domain/entity/PortfolioDetail.java
+++ src/main/java/co/fineants/api/domain/portfolio/domain/entity/PortfolioDetail.java
@@ -37,19 +37,14 @@ public class PortfolioDetail {
 	/**
 	 * Return a PortfolioDetail instance.
 	 *
-	 * @param name the name 포트폴리오 이름
-	 * @param securitiesFirm the securities firm 증권사 이름
-	 * @param properties the properties 증권사 목록이 담긴 포트폴리오 프로퍼티
-	 * @return the portfolio detail 포트폴리오 상세 정보 객체
-	 * @throws IllegalArgumentException 포트폴리오 이름이 형식에 유효하지 않거나 증권사 이름이 properties 목록에 포함되지 않으면 예외 발생한다
+	 * @param name 포트폴리오 이름
+	 * @param securitiesFirm 증권사 이름
+	 * @param properties 증권사 목록이 담긴 포트폴리오 프로퍼티
+	 * @return 포트폴리오 상세 정보 객체
+	 * @throws IllegalArgumentException 포트폴리오 이름이 형식에 유효하지 않거나 증권사 이름이 목록에 포함되지 않으면 예외 발생한다
 	 */
 	public static PortfolioDetail of(String name, String securitiesFirm, PortfolioProperties properties) {
 		return new PortfolioDetail(name, securitiesFirm, properties);
-	}
-
-	@Override
-	public String toString() {
-		return String.format("(name=%s, securitiesFirm=%s)", name, securitiesFirm);
 	}
 
 	public void change(PortfolioDetail detail) {
@@ -67,5 +62,10 @@ public class PortfolioDetail {
 
 	public String getMaximumLossReachMessage() {
 		return String.format("%s이(가) 최대 손실율에 도달했습니다", name);
+	}
+
+	@Override
+	public String toString() {
+		return String.format("(name=%s, securitiesFirm=%s)", name, securitiesFirm);
 	}
 }
