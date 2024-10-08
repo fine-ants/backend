15e0aed1df1c2d10e0b61a12480029031b21c309
YongHwan Kim <dragonbead95@naver.com>
2024-10-08 14:25:08 +0900

    feat: CookieDomainProvider 인터페이스 선언 및 구현클래스 추가 (#489)
    - 프로파일 환경별 도메인 이름을 제공하는 인터페이스 추가

--- src/main/java/co/fineants/api/global/security/factory/TokenFactory.java
+++ src/main/java/co/fineants/api/global/security/factory/TokenFactory.java
@@ -4,14 +4,19 @@ import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
 
 import co.fineants.api.global.security.oauth.dto.Token;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Component
+@RequiredArgsConstructor
 @Slf4j
 public class TokenFactory {
 
+	private final CookieDomainProvider provider;
+
 	public ResponseCookie createAccessTokenCookie(Token token) {
 		return token.createAccessTokenCookie()
+			.domain(provider.domain())
 			.sameSite("None")
 			.path("/")
 			.secure(true)
@@ -21,6 +26,7 @@ public class TokenFactory {
 
 	public ResponseCookie createRefreshTokenCookie(Token token) {
 		return token.createRefreshTokenCookie()
+			.domain(provider.domain())
 			.sameSite("None")
 			.path("/")
 			.secure(true)
