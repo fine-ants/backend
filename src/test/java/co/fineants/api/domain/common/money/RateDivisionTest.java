5cb496c8119ed6a8e8d993e1507d96787c50651e
yonghwankim-dev <dragonbead95@naver.com>
2024-10-18 16:39:30 +0900

    test: RateDivision 테스트 추가

---
+++ src/test/java/co/fineants/api/domain/common/money/RateDivisionTest.java
@@ -0,0 +1,22 @@
+package co.fineants.api.domain.common.money;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class RateDivisionTest {
+
+	@DisplayName("분자를 0으로 나누면 0원을 ")
+	@Test
+	void reduce_givenMoney_whenDivisorIsZero_thenThrowException() {
+		// given
+		Money division = Money.won(10000);
+		Money divisor = Money.zero();
+		Expression expression = RateDivision.of(division, divisor);
+		// when
+		Expression actual = expression.reduce(Bank.getInstance(), Currency.KRW);
+		// then
+		Assertions.assertThat(actual).isEqualByComparingTo(Money.ZERO);
+	}
+
+}
