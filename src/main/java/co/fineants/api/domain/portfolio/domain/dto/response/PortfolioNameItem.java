8ebc3ba4d8ceabc8d86f91bb4e1c36a4b672fd0a
yonghwankim-dev <dragonbead95@naver.com>
2024-10-22 15:59:35 +0900

    feat: 포트폴리오 이름 리스폰스에 직렬화 설정

--- src/main/java/co/fineants/api/domain/portfolio/domain/dto/response/PortfolioNameItem.java
+++ src/main/java/co/fineants/api/domain/portfolio/domain/dto/response/PortfolioNameItem.java
@@ -2,14 +2,19 @@ package co.fineants.api.domain.portfolio.domain.dto.response;
 
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import co.fineants.api.domain.portfolio.domain.entity.Portfolio;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public class PortfolioNameItem {
+	@JsonProperty
 	private final Long id;
+	@JsonProperty
 	private final String name;
+	@JsonProperty
 	private final LocalDateTime dateCreated;
 
 	public static PortfolioNameItem from(Portfolio portfolio) {
