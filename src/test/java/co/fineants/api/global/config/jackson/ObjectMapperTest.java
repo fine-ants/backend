055c74d57644d9773cdb9fb9d468d6df7041c4ec
yonghwankim-dev <dragonbead95@naver.com>
2024-10-22 14:22:14 +0900

    test: 직렬화 테스트 추가

---
+++ src/test/java/co/fineants/api/global/config/jackson/ObjectMapperTest.java
@@ -0,0 +1,35 @@
+package co.fineants.api.global.config.jackson;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import co.fineants.AbstractContainerBaseTest;
+import co.fineants.api.domain.holding.domain.dto.response.PortfolioStockCreateResponse;
+import co.fineants.api.domain.holding.domain.entity.PortfolioHolding;
+import co.fineants.api.domain.portfolio.domain.entity.Portfolio;
+import co.fineants.api.domain.stock.domain.entity.Stock;
+
+public class ObjectMapperTest extends AbstractContainerBaseTest {
+
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	@DisplayName("리스폰스 객체를 직렬화한다")
+	@Test
+	void givenResponse_whenSerialize_thenReturnStringOfResponse() throws JsonProcessingException {
+		// given
+		Portfolio portfolio = createPortfolio(createMember());
+		Stock samsung = createSamsungStock();
+		PortfolioHolding holding = createPortfolioHolding(portfolio, samsung);
+		PortfolioStockCreateResponse response = PortfolioStockCreateResponse.from(holding);
+		// when
+		String actual = objectMapper.writeValueAsString(response);
+		// then
+		System.out.println(actual);
+	}
+
+}
