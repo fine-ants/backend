b2f9c9861173a41cdab885a0e6766a7070af55c4
yonghwankim-dev <dragonbead95@naver.com>
2024-10-22 14:47:01 +0900

    fix: PortfolioStockCreateResponse의 필드에 JsonProperty 추가

--- src/main/java/co/fineants/api/domain/holding/domain/dto/response/PortfolioStockCreateResponse.java
+++ src/main/java/co/fineants/api/domain/holding/domain/dto/response/PortfolioStockCreateResponse.java
@@ -1,15 +1,23 @@
 package co.fineants.api.domain.holding.domain.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import co.fineants.api.domain.holding.domain.entity.PortfolioHolding;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public class PortfolioStockCreateResponse {
 
-	private Long portfolioHoldingId;
+	@JsonProperty
+	private final Long portfolioHoldingId;
 
 	public static PortfolioStockCreateResponse from(PortfolioHolding portFolioHolding) {
 		return new PortfolioStockCreateResponse(portFolioHolding.getId());
+	}
+
+	@Override
+	public String toString() {
+		return String.format("포트폴리오 종목 생성 응답 결과(portfolioHoldingId=%d)", portfolioHoldingId);
 	}
 }
