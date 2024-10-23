d88751722d63a01925a845e894a4b315e4453b9a
yonghwankim-dev <dragonbead95@naver.com>
2024-10-23 15:55:18 +0900

    feat: 포트폴리오 이름 목록 API에 대한 직렬화 설정

--- src/main/java/co/fineants/api/domain/portfolio/domain/dto/response/PortfolioNameItem.java
+++ src/main/java/co/fineants/api/domain/portfolio/domain/dto/response/PortfolioNameItem.java
@@ -2,13 +2,11 @@ package co.fineants.api.domain.portfolio.domain.dto.response;
 
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import co.fineants.api.domain.portfolio.domain.entity.Portfolio;
-import lombok.AccessLevel;
-import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public class PortfolioNameItem {
 	@JsonProperty
 	private final Long id;
@@ -16,6 +14,14 @@ public class PortfolioNameItem {
 	private final String name;
 	@JsonProperty
 	private final LocalDateTime dateCreated;
+
+	@JsonCreator
+	private PortfolioNameItem(@JsonProperty("id") Long id, @JsonProperty("name") String name,
+		@JsonProperty("dateCreated") LocalDateTime dateCreated) {
+		this.id = id;
+		this.name = name;
+		this.dateCreated = dateCreated;
+	}
 
 	public static PortfolioNameItem from(Portfolio portfolio) {
 		return new PortfolioNameItem(
