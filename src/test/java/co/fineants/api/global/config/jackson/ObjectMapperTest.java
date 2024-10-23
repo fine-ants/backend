99d993d2d774762a38f8566e9ea2a071531fb3da
yonghwankim-dev <dragonbead95@naver.com>
2024-10-23 13:24:06 +0900

    test: 직렬화 테스트 추가

--- src/test/java/co/fineants/api/global/config/jackson/ObjectMapperTest.java
+++ src/test/java/co/fineants/api/global/config/jackson/ObjectMapperTest.java
@@ -1,5 +1,7 @@
 package co.fineants.api.global.config.jackson;
 
+import java.util.Set;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -31,5 +33,24 @@ class ObjectMapperTest extends AbstractContainerBaseTest {
 		String actual = objectMapper.writeValueAsString(response);
 		// then
 		Assertions.assertThat(actual).contains("portfolioHoldingId");
+	}
+
+	// TODO: 캐시 설정, 레디스 설정, ObjectMapper 설정간에 에러가 없는지 테스트 추가
+	@DisplayName("캐시를 위한 직렬화기가 직렬화할때는 클래스 이름이 포함되어 있다")
+	@Test
+	void givenCacheObjectMapper_whenSerialization_thenContainsClassName() throws JsonProcessingException {
+		// given
+		ObjectMapper cacheObjectMapper = new ObjectMapper();
+		cacheObjectMapper.setConfig(objectMapper.getSerializationConfig());
+		cacheObjectMapper.activateDefaultTyping(
+			cacheObjectMapper.getPolymorphicTypeValidator(),
+			ObjectMapper.DefaultTyping.EVERYTHING
+		);
+		Set<String> tickerSymbols = Set.of("005930", "123456");
+		// when
+		String actual = cacheObjectMapper.writeValueAsString(tickerSymbols);
+		// then
+		Set expected = cacheObjectMapper.readValue(actual, Set.class);
+		Assertions.assertThat(tickerSymbols).isEqualTo(expected);
 	}
 }
