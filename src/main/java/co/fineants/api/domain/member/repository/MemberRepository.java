c19b74f599c2d572564282c8bae0041f3edb318d
yonghwankim-dev <dragonbead95@naver.com>
2024-10-25 13:36:48 +0900

    refactor: 회원가입 직렬화 설정

--- src/main/java/co/fineants/api/domain/member/repository/MemberRepository.java
+++ src/main/java/co/fineants/api/domain/member/repository/MemberRepository.java
@@ -14,12 +14,19 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 		+ "where m.profile.email = :email and m.profile.provider = :provider")
 	Optional<Member> findMemberByEmailAndProvider(@Param("email") String email, @Param("provider") String provider);
 
+	@Query("select distinct m from Member m join fetch m.roles "
+		+ "where m.profile.email = :#{#member.profile.email} and m.profile.provider = :provider")
+	Optional<Member> findMemberByEmailAndProvider(@Param("member") Member member, @Param("provider") String provider);
+
 	@Query("select m from Member m where m.profile.nickname = :nickname and m.id != :memberId")
 	Optional<Member> findMemberByNicknameAndNotMemberId(@Param("nickname") String nickname,
 		@Param("memberId") Long memberId);
 
 	@Query("select m from Member m where m.profile.nickname = :nickname")
 	Optional<Member> findMemberByNickname(@Param("nickname") String nickname);
+
+	@Query("select m from Member m where m.profile.nickname = :#{#member.profile.nickname}")
+	Optional<Member> findMemberByNickname(@Param("member") Member member);
 
 	@Modifying
 	@Query("update Member m set m.profile.password = :password where m.id = :id")
