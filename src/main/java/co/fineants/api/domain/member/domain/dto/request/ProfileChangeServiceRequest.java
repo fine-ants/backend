f216d0452bda68c3f07ac44e54ba6d2b7470cb60
yonghwankim-dev <dragonbead95@naver.com>
2024-10-25 13:02:19 +0900

    refactor: 프로필 변경 서비스 요청의 직렬화 수정

--- src/main/java/co/fineants/api/domain/member/domain/dto/request/ProfileChangeServiceRequest.java
+++ src/main/java/co/fineants/api/domain/member/domain/dto/request/ProfileChangeServiceRequest.java
@@ -2,23 +2,50 @@ package co.fineants.api.domain.member.domain.dto.request;
 
 import org.springframework.web.multipart.MultipartFile;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
-@Getter
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
-@ToString
+import io.jsonwebtoken.lang.Strings;
+import lombok.Getter;
+
 public class ProfileChangeServiceRequest {
-	private MultipartFile profileImageFile;
-	private String nickname;
-	private Long memberId;
+	@JsonProperty
+	@Getter
+	private final MultipartFile profileImageFile;
+	@JsonProperty
+	private final String nickname;
+	@JsonProperty
+	private final Long memberId;
+
+	@JsonCreator
+	public ProfileChangeServiceRequest(
+		@JsonProperty("profileImageFile") MultipartFile profileImageFile,
+		@JsonProperty("nickname") String nickname,
+		@JsonProperty("memberId") Long memberId) {
+		this.profileImageFile = profileImageFile;
+		this.nickname = nickname;
+		this.memberId = memberId;
+	}
 
 	public static ProfileChangeServiceRequest of(MultipartFile profileImageFile, String nickname,
 		Long memberId) {
 		return new ProfileChangeServiceRequest(profileImageFile, nickname, memberId);
+	}
+
+	public boolean hasNickname() {
+		return Strings.hasText(nickname);
+	}
+
+	public Long memberId() {
+		return memberId;
+	}
+
+	public String nickname() {
+		return nickname;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("ProfileChangeServiceRequest(nickname=%s, memberId=%d)", nickname, memberId);
 	}
 }
