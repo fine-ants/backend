9f4fa4dc5cfedfde354a05a1d6c2b5f8dfb3ff52
yonghwankim-dev <dragonbead95@naver.com>
2024-10-25 11:18:45 +0900

    refactor: LoginRequest 직렬화 설정

--- src/main/java/co/fineants/api/domain/member/domain/dto/request/LoginRequest.java
+++ src/main/java/co/fineants/api/domain/member/domain/dto/request/LoginRequest.java
@@ -1,20 +1,43 @@
 package co.fineants.api.domain.member.domain.dto.request;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.util.StringUtils;
 
-@Getter
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
-@ToString
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import co.fineants.api.global.security.ajax.token.AjaxAuthenticationToken;
+
 public class LoginRequest {
-	private String email;
-	private String password;
+	@JsonProperty
+	private final String email;
+	@JsonProperty
+	private final String password;
+
+	@JsonCreator
+	public LoginRequest(@JsonProperty("email") String email, @JsonProperty("password") String password) {
+		this.email = email;
+		this.password = password;
+	}
 
 	public static LoginRequest create(String email, String password) {
 		return new LoginRequest(email, password);
+	}
+
+	public AbstractAuthenticationToken toUnauthenticatedAjaxToken() {
+		return AjaxAuthenticationToken.unauthenticated(email, password);
+	}
+
+	public boolean hasEmail() {
+		return StringUtils.hasText(email);
+	}
+
+	public boolean hasPassword() {
+		return StringUtils.hasText(password);
+	}
+
+	@Override
+	public String toString() {
+		return String.format("LoginRequest(email=%s, password=%s)", email, password);
 	}
 }
