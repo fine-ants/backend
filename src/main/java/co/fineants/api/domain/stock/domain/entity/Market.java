d29ede5467c5f316f7f0f510e844430a99555c63
yonghwankim-dev <dragonbead95@naver.com>
2024-11-11 16:11:56 +0900

    feat: 시장 추가

--- src/main/java/co/fineants/api/domain/stock/domain/entity/Market.java
+++ src/main/java/co/fineants/api/domain/stock/domain/entity/Market.java
@@ -3,12 +3,28 @@ package co.fineants.api.domain.stock.domain.entity;
 import java.util.Arrays;
 
 public enum Market {
-	KOSPI("KOSPI"), KOSDAQ("KOSDAQ"), KONEX("KONEX"), KOSDAQ_GLOBAL("KOSDAQ GLOBAL"), NONE("NONE");
+	KOSPI("KOSPI", "코스피"),
+	KOSDAQ("KOSDAQ", "코스닥"),
+	KOSDAQ_GLOBAL("KOSDAQ GLOBAL", "코스닥 글로벌"),
+	KNX("KNX", "코넥스"),
+	AGR("AGR", "농축산물파생"),
+	BON("BON", "채권파생"),
+	CMD("CMD", "일반상품시장"),
+	CUR("CUR", "통화파생"),
+	ENG("ENG", "에너지파생"),
+	EQU("EQU", "주식파생"),
+	ETF("ETF", "ETF파생"),
+	IRT("IRT", "금리파생"),
+	MTL("MTL", "금속파생"),
+	SPI("SPI", "주가지수파생"),
+	NONE("NONE", "미분류");
 
 	private final String name;
+	private final String description;
 
-	Market(String name) {
+	Market(String name, String description) {
 		this.name = name;
+		this.description = description;
 	}
 
 	public static Market ofMarket(String dbData) {
@@ -18,12 +34,27 @@ public enum Market {
 			.orElse(NONE);
 	}
 
-	public static Market valueByMarketIdCode(String marketIdCode) {
+	public static Market valueBy(String marketIdCode) {
 		return switch (marketIdCode) {
 			case "STK" -> KOSPI;
 			case "KSQ" -> KOSDAQ;
-			case "KNX" -> KONEX;
+			case "KNX" -> KNX;
+			case "ETF" -> ETF;
+			case "AGR" -> AGR;
+			case "BON" -> BON;
+			case "CMD" -> CMD;
+			case "CUR" -> CUR;
+			case "ENG" -> ENG;
+			case "EQU" -> EQU;
+			case "IRT" -> IRT;
+			case "MTL" -> MTL;
+			case "SPI" -> SPI;
 			default -> NONE;
 		};
+	}
+
+	@Override
+	public String toString() {
+		return String.format("(name=%s, description=%s)", name, description);
 	}
 }
