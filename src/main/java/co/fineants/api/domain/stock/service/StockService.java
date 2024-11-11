d90a82d58dcb80494ce2dd19d23ad3a0e92e0114
yonghwankim-dev <dragonbead95@naver.com>
2024-11-11 15:50:55 +0900

    feat: 종목 초기화 API 구현

--- src/main/java/co/fineants/api/domain/stock/service/StockService.java
+++ src/main/java/co/fineants/api/domain/stock/service/StockService.java
@@ -1,5 +1,11 @@
 package co.fineants.api.domain.stock.service;
 
+import static co.fineants.api.infra.s3.service.AmazonS3StockService.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -13,6 +19,7 @@ import co.fineants.api.domain.stock.domain.dto.response.StockReloadResponse;
 import co.fineants.api.domain.stock.domain.dto.response.StockResponse;
 import co.fineants.api.domain.stock.domain.dto.response.StockSearchItem;
 import co.fineants.api.domain.stock.domain.entity.Stock;
+import co.fineants.api.domain.stock.parser.StockParser;
 import co.fineants.api.domain.stock.repository.StockQueryRepository;
 import co.fineants.api.domain.stock.repository.StockRepository;
 import co.fineants.api.global.errors.errorcode.StockErrorCode;
@@ -34,6 +41,7 @@ public class StockService {
 	private final StockQueryRepository stockQueryRepository;
 	private final StockAndDividendManager stockAndDividendManager;
 	private final StockDividendRepository stockDividendRepository;
+	private final StockParser stockParser;
 
 	@Transactional(readOnly = true)
 	public List<StockSearchItem> search(StockSearchRequest request) {
@@ -64,5 +72,28 @@ public class StockService {
 		amazonS3StockService.writeStocks(stockRepository.findAll());
 		amazonS3DividendService.writeDividends(stockDividendRepository.findAll());
 		return response;
+	}
+
+	// csv 파일을 입력받아서 데이터베이스와 aws의 stocks.csv로 저장
+	@Transactional
+	public void initStocks(InputStream inputStream) {
+		List<Stock> stocks;
+		// Stock 리스트로 파싱
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
+			stocks = br.lines()
+				.skip(1) // skip title
+				.map(CSV_SEPARATOR_PATTERN::split)
+				.map(stockParser::parse)
+				.distinct()
+				.toList();
+		} catch (IOException e) {
+			throw new IllegalArgumentException("fail parsing stock", e);
+		}
+		// 데이터베이스에 저장
+		stockRepository.deleteAllInBatch();
+		List<Stock> saveStocks = stockRepository.saveAll(stocks);
+
+		// aws에 저장
+		amazonS3StockService.writeStocks(saveStocks);
 	}
 }
